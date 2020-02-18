e669cb0fcbcf0e49a7b2215722af236daa59ff36
nirmal <nirmal495@gmail.com>
2020-02-18 18:50:57 +0530

    updated services and added cors

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,6 @@
 #import libraries
 from flask import Flask, request
+from flask_cors import CORS
 import json
 import os
 import glob
@@ -15,6 +16,7 @@ aqg = aqgFunction.AutomaticQuestionGenerator()
 nest_asyncio.apply()
 
 app = Flask(__name__)
+CORS(app)
 
 
 #establishing  paths of the rasa bot files
@@ -42,7 +44,7 @@ sentences= []
 term = dict()
 for line in text:
     if "##" and ":" and "intent" in line:
-        
+
         term[intent] = sentences
         sentences=[]
         intent = line.replace("##",'')
@@ -76,7 +78,7 @@ for keys in data2:
     data4=data3[0]
     data5 = data4["text"]
     newdict[keys]= data5
-    
+
 dictrand={}
 with open(stories_path, 'r') as h:
     text2 = h.readlines()
@@ -87,14 +89,14 @@ for line8 in text2:
         line9= line9.strip()
         if line9 in list(term.keys()):
             first = line9
-            
+
     if "##" not in line8 and "*" not in line8 and "-" in line8:
         line6 = line8.replace("-","")
         line6 = line6.strip()
         if line6 in list(newdict.keys()):
             second = line6
             dictrand[first]= second
-            
+
 
 
 
@@ -107,7 +109,7 @@ def predict():
     asyncio.set_event_loop(loop)
     Prediction = asyncio.run(agent.parse_message_using_nlu_interpreter(message_data=query, tracker=None))
     Prediction = Prediction["intent"]['name']
-    
+
     return Prediction
 
 
@@ -119,31 +121,31 @@ def newintent():
     intent_name = jsonObject['name_intent']
     questions = jsonObject['questionList']
     response = jsonObject['respond']
-    
+
     if len(intent_name) == 0 or len(response) == 0:
-        
+
         return {"message": "enter required fields"}
-        
+
     elif len(questions)<5 :
-        
+
         return {"message": "Number of Questions not sufficient"}
-    
+
     else:
-        
+
         if intent_name == 'default' or intent_name in list(term.keys()) or "intent" in intent_name:
-            
+
             return {"message": "Intent Name Not Accepted"}
         else:
-            
+
             intent = intent_name
             response = response
             action = "utter_"+intent
             Questions = questions
-            
+
             term[intent] = Questions
             newdict[action] = response
             dictrand[intent] = action
-            
+
             file_handler = open(nlu_path,'w')
             for finalkeys in list(term.keys()):
 
@@ -156,16 +158,16 @@ def newintent():
 
                 file_handler.write('\n')
             file_handler.close()
-            
+
             dictaction= dict()
             for k,v in newdict.items():
                 dictaction[k] = [{"text" : v}]
-            
+
             finaldict = {'actions': list(newdict.keys()), "intents" : list(term.keys()), "templates": dictaction}
-            
+
             with open(domain_path, 'w') as file:
                 yaml.dump(finaldict, file)
-            
+
             file_handler = open(stories_path,'w')
             for key2 in dictrand:
                 file_handler.write('\n'+ "## " + "path_" + key2 )
@@ -174,18 +176,18 @@ def newintent():
 
                 file_handler.write('\n')
             file_handler.close()
-            
+
             return {"message": "Intent Added"}
 
-        
-#remove intent service        
+
+#remove intent service
 @app.route("/removeintent" , methods=['POST'])
 def Rem1():
     global term, newdict, dictrand
     jsonObject = json.loads(request.data)
     intent_name = jsonObject['name_intent']
-    
-    
+
+
     if intent_name=='':
         return {'message': 'Please Enter Intent.'}
     else:
@@ -194,7 +196,7 @@ def Rem1():
         del term[req1]
         del newdict[req2]
         del dictrand[req1]
-        
+
         file_handler = open(nlu_path,'w')
         for finalkeys in list(term.keys()):
 
@@ -225,24 +227,24 @@ def Rem1():
 
             file_handler.write('\n')
         file_handler.close()
-        
-        return {"message": "intent removed"}             
 
-        
-#model training service        
+        return {"message": "intent removed"}
+
+
+#model training service
 @app.route("/train" , methods=['POST'])
 def train_model():
     global agent
     #os.chdir(original_path)
     asyncio.set_event_loop(asyncio.new_event_loop())
     train(domain= domain_path, config= config_path, training_files= train_path, force_training=False)
-    
-    list_of_files = glob.glob(models_path + '/*') 
+
+    list_of_files = glob.glob(models_path + '/*')
     latest_file = max(list_of_files, key=os.path.getctime)
     modelpath1 = os.path.abspath(latest_file)
-    
+
     agent = Agent.load(modelpath1)
-    
+
     return {"message": "training done"}
 
 
@@ -255,13 +257,13 @@ def Add():
     intent_name = jsonObject['name_intent']
     component = jsonObject['component']
     list3 = term[intent_name]
-    
+
     if len(component) == 0 :
-        
+
         return {"message": "select required fields"}
-    
+
     list3.append(component)
-    
+
     tup = list3
     term[intent_name] = tup
     file_handler = open(nlu_path,'w')
@@ -276,12 +278,12 @@ def Add():
 
         file_handler.write('\n')
     file_handler.close()
-    
+
     current = list()
     current.append(component)
     return {'message' : 'Component added'}
-    
-    
+
+
 
 #removing sentence from intent service
 @app.route("/RemoveComponent" , methods=['POST'])
@@ -290,18 +292,18 @@ def Rem():
     jsonObject = json.loads(request.data)
     component = jsonObject['component']
     intent_name = jsonObject['name_intent']
-    
-    
+
+
     if len(component) == 0 :
-        
+
         return {"message":"please enter required fields"}
     else:
-        
+
         pick8 = term[intent_name]
         if component in pick8:
             pick8.remove(component)
             term[intent_name] = pick8
-            
+
             file_handler = open(nlu_path,'w')
             for finalkeys in list(term.keys()):
 
@@ -315,11 +317,11 @@ def Rem():
                 file_handler.write('\n')
             file_handler.close()
             return {"message":"Component removed"}
-        
+
         else:
             return {'message':'Sentence not present'}
-            
-    
+
+
 
 #generate questions from corpus service
 @app.route("/corpusQuestions" , methods=['POST'])
@@ -329,39 +331,27 @@ def corpus():
     questionList = aqg.aqgParse(inputText)
     for x in range(questionList.count('\n')):
         questionList.remove('\n')
-    
+
     return {"questions": questionList}
 
 
 
 
 #get intent list
-@app.route("/intentList", methods=['POST'])
-def intentList():
-    list1= list(term.keys())   
-    return {"intents": list1}
-
-
+@app.route("/getIntentList", methods=['GET'])
+def getIntentList():
+    return json.dumps(list(term.keys()))
 
 #getQuestions and Response for an intent
-@app.route("/QuestionsAndResponse", methods=['POST'])
-def QuestionsAndResponse():
+@app.route("/getQuestionsAndAnswer", methods=['POST'])
+def getQuestionsAndAnswer():
     jsonObject = json.loads(request.data)
-    intentName = jsonObject['intent_name']
+    intentName = jsonObject['intentName']
     QuestionList = term[intentName]
-    interm = "utter_"+intentName
-    response = newdict[interm]    
-    return {"Qlist": QuestionList,"Response":response}
+    interm = "utter_" + intentName
+    print(type(newdict))
+    response = newdict.get(interm)
+    if response == 'None':
+        response = ""
 
-    
-    
-    
-
-
-
-
-    
-    
-    
-    
-    
+    return {"questions": QuestionList, "answer": response}
