989833fbc33ac7a88f0ec59ea801d8ddae05d261
Fahad ALi Shaikh <fshaikh@digite.com>
2020-03-23 13:34:30 +0530

    done changes for ChatHistory to load intent with confidence and bot_utterance name

--- bot_trainer/history.py
+++ bot_trainer/history.py
@@ -7,10 +7,22 @@ class ChatHistory:
 
     def fetch_chat_history(self, sender):
         events = self.tracker.retrieve(sender).as_dialogue().events
-        for event in events:
+        bot_utterance = None
+        for i in range(events.__len__()):
+            event = events[i]
             event_data = event.as_dict()
-            if event_data['event'] in ['user', 'bot']:
-                yield {'event': event_data['event'], 'text': event_data['text']}
+            if event_data['event'] in  ['user', 'bot']:
+                result = {'event': event_data['event'], 'text': event_data['text'], 'timestamp' : event_data['timestamp']}
+                if event_data['event']  == 'user':
+                    parse_data = event_data['parse_data']
+                    result['intent'] = parse_data['intent'] ['name']
+                    result['confidence'] = parse_data['intent']['confidence']
+                elif event_data['event'] == 'bot':
+                        if bot_utterance:
+                            result['response'] = bot_utterance
+                yield result
+            else:
+                bot_utterance = event_data['name'] if event_data['event'] == 'action' else None
 
     def fetch_chat_users(self):
         return self.tracker.keys()
