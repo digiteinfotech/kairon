94ddf2e3a23bbf8446f86ea6da5167914eb816fc
Fahad ALi Shaikh <fshaikh@digite.com>
2020-03-31 16:21:07 +0530

    1. done changes in docker-compose.yml 3. added metrics in history.py

--- bot_trainer/history.py
+++ bot_trainer/history.py
@@ -1,5 +1,7 @@
 from rasa.core.tracker_store import MongoTrackerStore, DialogueStateTracker
 from rasa.core.domain import Domain
+import pandas as pd
+from datetime import datetime
 
 class ChatHistory:
     def __init__(self, domainFile: str, mongo_url :str, mongo_db = 'conversation'):
@@ -19,7 +21,7 @@ class ChatHistory:
             event = events[i]
             event_data = event.as_dict()
             if event_data['event'] not in ['action', 'rewind']:
-                result = {'event': event_data['event'], 'timestamp': event_data['timestamp']}
+                result = {'event': event_data['event'], 'time': datetime.fromtimestamp(event_data['timestamp']).time(), 'date': datetime.fromtimestamp(event_data['timestamp']).date()}
 
                 if event_data['event'] not in ['session_started', 'rewind']:
                     result['text'] = event_data['text']
@@ -38,16 +40,71 @@ class ChatHistory:
             else:
                 bot_action = event_data['name'] if event_data['event'] == 'action' else None
 
-    def __fetch_history_for_metrics(self):
-        records = self.tracker.conversations.find()
-        for record in records:
-            events = record['events']
-            sender_id = record['sender_id']
-
-
     def __fetch_user_history(self, sender_id, latest_history = True):
         if latest_history:
             return self.tracker.retrieve(sender_id).as_dialogue().events
         else:
             user_conversation = self.tracker.conversations.find_one({"sender_id": sender_id})
             return DialogueStateTracker.from_dict(sender_id, list(user_conversation['events']), self.domain.slots).as_dialogue().events
+
+    def visitor_hit_fallback(self):
+        data_frame = self.__fetch_history_metrics()
+        fallback_count = data_frame[data_frame['name'] == 'action_default_fallback'].count()['name']
+        total_count = data_frame.count()['name']
+        return { 'fallback_count': fallback_count, 'total_count' : total_count  }
+
+    def conversation_steps(self):
+        '''
+        data_frame = data_frame.groupby(['sender_id', data_frame.event.ne('session_started')])
+        data_frame = data_frame.size().reset_index()
+        data_frame.rename(columns={0: 'count'}, inplace=True)
+        data_frame = data_frame[data_frame['event'] != 0]
+        return data_frame.to_json(orient='records')
+        '''
+        data_frame = self.__fetch_history_metrics()
+        data_frame['prev_event'] = data_frame['event'].shift()
+        data_frame['prev_timestamp'] = data_frame['timestamp'].shift()
+        data_frame.fillna('', inplace=True)
+        data_frame = data_frame[((data_frame['event'] == "bot") & (data_frame['prev_event'] == 'user'))]
+        return data_frame.groupby(['sender_id']).count().reset_index()[['sender_id', 'event']].to_json(orient='records')
+
+    def conversation_steps(self):
+        '''
+        data_frame = data_frame.groupby(['sender_id', data_frame.event.ne('session_started')])
+        data_frame = data_frame.size().reset_index()
+        data_frame.rename(columns={0: 'count'}, inplace=True)
+        data_frame = data_frame[data_frame['event'] != 0]
+        return data_frame.to_json(orient='records')
+        '''
+        data_frame = self.__fetch_history_metrics()
+        data_frame['prev_event'] = data_frame['event'].shift()
+        data_frame['prev_timestamp'] = data_frame['timestamp'].shift()
+        data_frame.fillna('', inplace=True)
+        data_frame = data_frame[((data_frame['event'] == "bot") & (data_frame['prev_event'] == 'user'))]
+        return data_frame.groupby(['sender_id']).count().reset_index()[['sender_id', 'event']].to_json(orient='records')
+
+    def conversation_time(self):
+        data_frame = self.__fetch_history_metrics()
+        data_frame['prev_event'] = data_frame['event'].shift()
+        data_frame['prev_timestamp'] = data_frame['timestamp'].shift()
+        data_frame.fillna('', inplace=True)
+        data_frame = data_frame[((data_frame['event'] == "bot") & (data_frame['prev_event'] == 'user'))]
+        data_frame['time'] = pd.to_datetime(data_frame['timestamp'], unit='s') - pd.to_datetime(data_frame['prev_timestamp'], unit='s')
+        return data_frame[['sender_id', 'time']].groupby('sender_id').sum().reset_index().to_json(orient='records')
+
+    def __fetch_history_metrics(self, show_session=False, filter_columns=None):
+        filter_events = ['user', 'bot']
+        if show_session:
+            filter_events.append('session_started')
+
+        if not filter_columns:
+            filter_columns = ['sender_id', 'event', 'name', 'text', 'timestamp', 'input_channel', 'message_id']
+        records = self.tracker.conversations.find()
+        data_frame = pd.DataFrame(list(records))
+        data_frame = data_frame.explode(column='events')
+        data_frame = pd.concat([data_frame.drop(['events'], axis=1), data_frame['events'].apply(pd.Series)], axis=1)
+        data_frame.fillna('', inplace=True)
+        data_frame['name'] = data_frame['name'].shift()
+        data_frame = data_frame[data_frame['event'].isin(filter_events)]
+        data_frame = data_frame[filter_columns]
+        return data_frame
