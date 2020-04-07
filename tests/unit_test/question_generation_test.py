9dc8ae66ad74a071c4ae096c285644f382e327fc
Fahad ALi Shaikh <fshaikh@digite.com>
2020-04-07 09:02:20 +0530

    1. corrected question_generation_test result 2. added install all dependencies in gitlab.yml

--- tests/unit_test/question_generation_test.py
+++ tests/unit_test/question_generation_test.py
@@ -1,12 +1,12 @@
 import asyncio
-
+import pytest
 from bot_trainer.QuestionGeneration import QuestionGeneration
 
 
 class TestQuestionGeneration:
 
     def test_generate_questions(self):
-        result = ['where is digite stationed ?', 'where is digite constructed ?', 'where is digite resided ?', 'where is digite headquartered ?', 'where is digite situated ?', 'where is digite operated ?', 'where is digite resides ?', 'where is digite housed ?', 'where is digite sited ?', 'where is digite positioned ?']
+        expected = ['where is digite stationed ?', 'where is digite constructed ?', 'where is digite resided ?', 'where is digite headquartered ?', 'where is digite situated ?', 'where is digite operated ?', 'where is digite resides ?', 'where is digite housed ?', 'where is digite sited ?', 'where is digite positioned ?']
         loop = asyncio.new_event_loop()
-        response = loop.run_until_complete(QuestionGeneration.generateQuestions('where is digite located?'))
-        assert response == result
+        actual = loop.run_until_complete(QuestionGeneration.generateQuestions('where is digite located?'))
+        assert all([a == b for a, b in zip(actual, expected)])
