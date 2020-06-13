c2926cc01f50862a6dd5d86438daddd105937b74
Fahad Ali Shaikh <fshaikh@digite.com>
2020-06-13 14:23:58 +0530

    expose headers content-disposition changes to small case

--- bot_trainer/api/app/main.py
+++ bot_trainer/api/app/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["Content-Disposition"]
+    expose_headers=["content-disposition"]
 )
 
 
