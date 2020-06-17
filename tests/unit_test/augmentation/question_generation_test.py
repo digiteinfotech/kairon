90beeeb20d9125e7de6dc599d1dd78001a314e1f
Fahad Ali Shaikh <fshaikh@digite.com>
2020-06-17 16:28:43 +0530

    test changes done

--- tests/unit_test/augmentation/question_generation_test.py
+++ tests/unit_test/augmentation/question_generation_test.py
@@ -6,7 +6,7 @@ from augmentation.generator import QuestionGenerator
 class TestQuestionGeneration:
 
     def test_generate_questions(self):
-        expected  = "where was digite located ?"
+        expected  = "where is digite now ?"
         loop = asyncio.new_event_loop()
         actual = loop.run_until_complete(QuestionGenerator.generateQuestions('where is digite located?'))
         print(actual)
