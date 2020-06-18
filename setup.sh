010bbb83b062dc82d2cc33fd242936deb3547c79
Fahad Ali Shaikh <fshaikh@digite.com>
2020-06-18 17:58:58 +0530

    internal fixes

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m spacy download en_core_web_md
 python -m spacy link en_core_web_md en
+python -m nltk.downloader wordnet
