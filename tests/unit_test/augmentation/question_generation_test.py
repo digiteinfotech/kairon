645f7ad49998f4acd50dbdcb76b181cabb27402a
Shashank M <shashank.m@digite.com>
2020-06-18 11:51:51 +0530

    Wordnet algorithm added

--- tests/unit_test/augmentation/question_generation_test.py
+++ tests/unit_test/augmentation/question_generation_test.py
@@ -6,8 +6,10 @@ from augmentation.generator import QuestionGenerator
 class TestQuestionGeneration:
 
     def test_generate_questions(self):
-        expected  = ["where is digite now ?", "where is it located ?", "how is digite located ?"]
+        expected = ["where is digite now ?", "where is it located ?", "how is digite located ?",'ally']
         loop = asyncio.new_event_loop()
         actual = loop.run_until_complete(QuestionGenerator.generateQuestions('where is digite located?'))
+        actual_token = loop.run_until_complete(QuestionGenerator.generateQuestions('friend'))
         print(actual)
-        assert any(text in expected for text in  actual)
+        print(actual_token)
+        assert any(text in expected for text in  actual) and expected[3] in actual_token
