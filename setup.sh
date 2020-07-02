cde1984310b9dd46828a8d18a7e938b0eb7d2ede
Fahad Ali Shaikh <fshaikh@digite.com>
2020-07-02 10:52:37 +0530

    removing python3.6

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3.6 -m pip install --upgrade pip
-python3.6 -m pip install -r requirements.txt
-python3.6 -m spacy download en_core_web_md
-python3.6 -m spacy link en_core_web_md en
-python3.6 -m nltk.downloader wordnet
-python3.6 -m nltk.downloader averaged_perceptron_tagger
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m spacy download en_core_web_md
+python -m spacy link en_core_web_md en
+python -m nltk.downloader wordnet
+python -m nltk.downloader averaged_perceptron_tagger
