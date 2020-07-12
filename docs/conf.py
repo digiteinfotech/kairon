bbac394847991f9c94446f6554d1e1a2f1789b23
ash-pramila <pramilap0123@gmail.com>
2020-07-13 02:46:54 +0530

    Added logo in the sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,8 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "collapse_navigation" : False
 }
+html_logo = 'chironlogo.svg'
+html_favicon = 'chironlogo.svg'
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
