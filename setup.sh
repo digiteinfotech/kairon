2b1659e1c21696bbf8d5491df9a79fe5bc4d76a3
sfahad2526 <shaikhfahad2526@gmail.com>
2020-12-03 12:17:14 +0530

    removed setuptools

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python -m pip install pip==20.2.4
-python -m pip install -U pip setuptools
 python -m pip install -r requirements.txt
 python -m pip install git+https://github.com/sfahad1414/question_generation.git
 python -m spacy download en_core_web_md
