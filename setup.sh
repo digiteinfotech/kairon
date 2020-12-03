49b6edb816d28b087f6932926880eff455f66cb4
sfahad2526 <shaikhfahad2526@gmail.com>
2020-12-03 12:13:50 +0530

    1. change pip version to 20.2.4 as version 20.3.0 causes infinite loop

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python -m pip install --upgrade pip
+python -m pip install pip==20.2.4
 python -m pip install -U pip setuptools
 python -m pip install -r requirements.txt
 python -m pip install git+https://github.com/sfahad1414/question_generation.git
