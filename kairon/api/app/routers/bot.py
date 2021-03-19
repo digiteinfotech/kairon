4751341390368566428689bc5695a05b7e691875
siddhant_intern <siddhant_intern@digite.com>
2021-03-19 11:48:02 +0530

    Fixed responses' case sensitivity issue

--- kairon/api/app/routers/bot.py
+++ kairon/api/app/routers/bot.py
@@ -197,7 +197,7 @@ async def get_responses(
     Fetches list of utterances against utterance name
     """
     return {
-        "data": list(mongo_processor.get_response(utterance, current_user.get_bot()))
+        "data": list(mongo_processor.get_response(utterance.lower(), current_user.get_bot()))
     }
 
 
@@ -211,7 +211,7 @@ async def add_responses(
     Adds utterance value in particular utterance
     """
     utterance_id = mongo_processor.add_text_response(
-        request_data.data, utterance, current_user.get_bot(), current_user.get_user()
+        request_data.data, utterance.lower(), current_user.get_bot(), current_user.get_user()
     )
     return {"message": "Utterance added!", "data": {"_id": utterance_id}}
 
@@ -229,7 +229,7 @@ async def edit_responses(
     mongo_processor.edit_text_response(
         id,
         request_data.data,
-        utterance,
+        utterance.lower(),
         current_user.get_bot(),
         current_user.get_user(),
     )
@@ -249,7 +249,7 @@ async def remove_responses(
     """
     if delete_utterance:
         mongo_processor.delete_utterance(
-            request_data.data, current_user.get_bot(), current_user.get_user()
+            request_data.data.lower(), current_user.get_bot(), current_user.get_user()
         )
     else:
         mongo_processor.delete_response(
