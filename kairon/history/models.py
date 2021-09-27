9dcc58d3293b22a6ef3bba6ea11e3afaf2860b7c
udit-pandey <pandey.udit867@gmail.com>
2021-09-27 22:51:34 +0530

    1. removed class attributes that were read from environment 2. added Bearer keyword for JWT authorization 3. modified query to take month between 1 and 6 4. modified tests

--- kairon/history/models.py
+++ kairon/history/models.py
@@ -3,7 +3,7 @@ from pydantic.main import BaseModel
 
 
 class HistoryQuery(BaseModel):
-    month: int = Query(default=1, ge=2, le=6)
+    month: int = Query(default=1, ge=1, le=6)
     conversation_step_threshold: int = Query(default=10, ge=2)
     action_fallback: str = Query(default="action_default_fallback")
     nlu_fallback: str = Query(default=None)
