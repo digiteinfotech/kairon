a152dbc1bacacf83b1f4d9a6acadd50316652f14
Shashank M <49338584+Shashank411@users.noreply.github.com>
2021-11-15 12:22:08 +0530

    fallback dropoff (#381)
    * fallback dropoff
    
    * additional bug fix

--- kairon/api/app/routers/history.py
+++ kairon/api/app/routers/history.py
@@ -331,3 +331,16 @@ async def conversation_time_trend(month: int = Query(default=6, ge=2, le=6), cur
         f'/api/history/{current_user.get_bot()}/trends/conversations/time',
         {'month': month}
     )
+
+
+@router.get("/metrics/user/fallback/dropoff", response_model=Response)
+async def fallback_dropoff(month: int = Query(default=1, ge=1, le=6), current_user: User = Depends(Authentication.get_current_user_and_bot)):
+    """
+    Fetches the list of users that dropped off after encountering fallback
+    """
+    fallback_action, nlu_fallback_action = DataUtility.load_fallback_actions(current_user.get_bot())
+    return Utility.trigger_history_server_request(
+        current_user.get_bot(),
+        f'/api/history/{current_user.get_bot()}/metrics/fallback/dropoff',
+        {'month': month, 'action_fallback': fallback_action, 'nlu_fallback': nlu_fallback_action}
+    )
--- kairon/history/router/metrics.py
+++ kairon/history/router/metrics.py
@@ -114,3 +114,13 @@ async def user_input_count(request: HistoryQuery = HistoryQuery(),
         collection, request.month
     )
     return {"data": user_inputs, "message": message}
+
+
+@router.get("/fallback/dropoff", response_model=Response)
+async def fallback_dropoff(request: HistoryQuery = HistoryQuery(),
+                           collection: str = Depends(Authentication.authenticate_and_get_collection)):
+    """Fetches the list of users that dropped off after encountering fallback."""
+    user_list, message = HistoryProcessor.user_fallback_dropoff(
+        collection, request.month, request.action_fallback, request.nlu_fallback
+    )
+    return {"data": user_list, "message": message}
