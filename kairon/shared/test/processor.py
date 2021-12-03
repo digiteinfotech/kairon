dc21a15ade7e039c147448ea2c37889216e8bf4c
udit-pandey <pandey.udit867@gmail.com>
2021-12-03 09:52:10 +0530

    fixed model testing issues

--- kairon/shared/test/processor.py
+++ kairon/shared/test/processor.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from bson import ObjectId
 from loguru import logger
 from mongoengine import Q, DoesNotExist
 
@@ -11,56 +12,56 @@ from kairon.shared.test.data_objects import ModelTestingLogs
 class ModelTestingLogProcessor:
 
     @staticmethod
-    def add_initiation_log(bot: str, user: str, run_e2e: bool, event_status=EVENT_STATUS.INITIATED.value):
-        ModelTestingLogs(
-            run_on_test_stories=run_e2e,
-            bot=bot,
-            user=user,
-            start_timestamp=datetime.utcnow(),
-            event_status=event_status
-        ).save()
-
-    @staticmethod
-    def update_log_with_test_results(bot: str, user: str, run_e2e: bool = None, event_status: str = None,
-                                     stories=None, nlu=None,
-                                     exception: str = None
-                                     ):
+    def log_test_result(bot: str, user: str, event_status: str = None,
+                        stories_result=None, nlu_result=None, exception: str = None):
         try:
-            doc = ModelTestingLogs.objects(bot=bot).filter(
+            common_data = ModelTestingLogs.objects(bot=bot, type='common').filter(
                 Q(event_status__ne=EVENT_STATUS.COMPLETED.value) &
                 Q(event_status__ne=EVENT_STATUS.FAIL.value)).get()
         except DoesNotExist:
-            doc = ModelTestingLogs(
+            common_data = ModelTestingLogs(
+                reference_id=ObjectId().__str__(),
+                type='common',
                 bot=bot,
                 user=user,
-                start_timestamp=datetime.utcnow(),
+                start_timestamp=datetime.utcnow()
             )
-        if run_e2e:
-            doc.run_on_test_stories = run_e2e
         if event_status:
-            doc.event_status = event_status
+            common_data.event_status = event_status
         if event_status in {EVENT_STATUS.FAIL.value, EVENT_STATUS.COMPLETED.value}:
-            doc.end_timestamp = datetime.utcnow()
+            common_data.end_timestamp = datetime.utcnow()
         if event_status == EVENT_STATUS.COMPLETED.value:
-            doc.status = 'PASSED'
-        if stories:
-            doc.stories = stories
-            if stories.get('failed_stories'):
-                doc.status = 'FAILURE'
-        if nlu:
-            doc.nlu = nlu
-            if nlu.get("intent_evaluation") and nlu["intent_evaluation"].get('errors'):
-                doc.status = 'FAILURE'
-            if nlu.get("response_selection_evaluation") and nlu["response_selection_evaluation"].get('errors'):
-                doc.status = 'FAILURE'
-            if nlu.get("entity_evaluation"):
-                for extractor_evaluation in nlu["entity_evaluation"].values():
+            common_data.status = 'PASSED'
+        if stories_result:
+            common_data.status = 'FAILURE' if stories_result.get('failed_stories') else 'SUCCESS'
+            ModelTestingLogs(
+                reference_id=common_data.reference_id,
+                type='stories',
+                bot=bot,
+                user=user,
+                data=stories_result
+            ).save()
+
+        if nlu_result:
+            ModelTestingLogs(
+                reference_id=common_data.reference_id,
+                type='nlu',
+                bot=bot,
+                user=user,
+                data=nlu_result
+            ).save()
+            if nlu_result.get("intent_evaluation") and nlu_result["intent_evaluation"].get('errors'):
+                common_data.status = 'FAILURE'
+            if nlu_result.get("response_selection_evaluation") and nlu_result["response_selection_evaluation"].get('errors'):
+                common_data.status = 'FAILURE'
+            if nlu_result.get("entity_evaluation"):
+                for extractor_evaluation in nlu_result["entity_evaluation"].values():
                     if extractor_evaluation.get('errors'):
-                        doc.status = 'FAILURE'
+                        common_data.status = 'FAILURE'
                         break
         if exception:
-            doc.exception = exception
-        doc.save()
+            common_data.exception = exception
+        common_data.save()
 
     @staticmethod
     def is_event_in_progress(bot: str, raise_exception=True):
@@ -73,7 +74,7 @@ class ModelTestingLogProcessor:
         """
         in_progress = False
         try:
-            ModelTestingLogs.objects(bot=bot).filter(
+            ModelTestingLogs.objects(bot=bot, type='common').filter(
                 Q(event_status__ne=EVENT_STATUS.COMPLETED.value) &
                 Q(event_status__ne=EVENT_STATUS.FAIL.value)).get()
 
@@ -95,9 +96,9 @@ class ModelTestingLogProcessor:
         from kairon import Utility
 
         today = datetime.today()
-        today_start = today.replace(hour=0, minute=0, second=0)
+        initiated_today = today.replace(hour=0, minute=0, second=0)
         doc_count = ModelTestingLogs.objects(
-            bot=bot, start_timestamp__gte=today_start
+            bot=bot, type='common', start_timestamp__gte=initiated_today
         ).count()
         if doc_count >= Utility.environment['model']["test"]["limit_per_day"]:
             if raise_exception:
@@ -114,8 +115,15 @@ class ModelTestingLogProcessor:
         @param bot: bot id.
         @return: list of logs.
         """
-        for log in ModelTestingLogs.objects(bot=bot).order_by("-start_timestamp"):
-            log = log.to_mongo().to_dict()
-            log.pop('_id')
-            log.pop('bot')
-            yield log
+        return list(ModelTestingLogs.objects(bot=bot).aggregate([
+            {"$set": {"data.type": "$type"}},
+            {'$group': {'_id': '$reference_id', 'bot': {'$first': '$bot'}, 'user': {'$first': '$user'},
+                        'status': {'$first': '$status'},
+                        'event_status': {'$first': '$event_status'}, 'data': {'$push': '$data'},
+                        'exception': {'$first': '$exception'},
+                        'start_timestamp': {'$first': '$start_timestamp'},
+                        'end_timestamp': {'$first': '$end_timestamp'}}},
+            {'$project': {
+                'data': {'$filter': {'input': '$data', 'as': 'data', 'cond': {'$ne': ['$$data.type', 'common']}}},
+                'status': 1, 'event_status': 1, 'exception': 1, 'start_timestamp': 1, 'end_timestamp': 1}},
+            {"$sort": {"start_timestamp": -1}}]))
