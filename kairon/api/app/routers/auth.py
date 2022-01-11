b23762b9d570e7dace84b9250f181db506daea31
udit-pandey <pandey.udit867@gmail.com>
2022-01-11 19:18:55 +0530

    1. sso signup changes 2. mail template for informing user password 3. added/updated unit and integration tests

--- kairon/api/app/routers/auth.py
+++ kairon/api/app/routers/auth.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Security, Path
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from starlette.background import BackgroundTasks
 from starlette.requests import Request
 
+from kairon import Utility
 from kairon.shared.auth import Authentication, LoginSSOFactory
 from kairon.api.models import Response
 from kairon.shared.constants import ADMIN_ACCESS
@@ -40,6 +42,14 @@ async def generate_integration_token(
     }
 
 
+@router.get("/login/sso/list/enabled", response_model=Response)
+async def sso_callback():
+    """
+    List social media logins enabled.
+    """
+    return Response(data=Utility.get_enabled_sso())
+
+
 @router.get('/login/sso/{sso_type}')
 async def sso_login(sso_type: str = Path(default=None, description="social media type", example="google")):
     """
@@ -49,14 +59,22 @@ async def sso_login(sso_type: str = Path(default=None, description="social media
 
 
 @router.get("/login/sso/callback/{sso_type}", response_model=Response)
-async def sso_callback(request: Request, sso_type: str = Path(default=None,
-                                                              description="social media type", example="google")):
+async def sso_callback(
+        request: Request,
+        background_tasks: BackgroundTasks,
+        sso_type: str = Path(default=None, description="social media type", example="google")
+):
     """
     Generate login token after successful social media login.
     """
-    access_token = await LoginSSOFactory.verify_and_process(request, sso_type)
+    existing_user, user_details, access_token = await LoginSSOFactory.verify_and_process(request, sso_type)
+    if not existing_user and Utility.email_conf["email"]["enable"]:
+        background_tasks.add_task(
+            Utility.format_and_send_mail, mail_type='password_generated', email=user_details['email'],
+            first_name=user_details['first_name'], password=user_details['password'].get_secret_value()
+        )
     return {
         "data": {"access_token": access_token, "token_type": "bearer"},
         "message": """It is your responsibility to keep the token secret.
-            If leaked then other may have access to your system.""",
+        If leaked then other may have access to your system.""",
     }
