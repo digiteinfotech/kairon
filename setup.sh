e737cd21947f86c554d308ea3cb89c1e490c2b5c
fshaikh <fshaikh@digite.com>
2022-01-25 15:30:14 +0530

    added nltk multi-lingual wordnet

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ python -m nltk.downloader wordnet
 python -m nltk.downloader averaged_perceptron_tagger
 python -m nltk.downloader punkt
 python -m nltk.downloader stopwords
+python -m nltk.downloader omw-1.4
