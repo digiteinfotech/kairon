019881dd2a7a8040b0a885211bd2d85ada1b4c90
udit-pandey <pandey.udit867@gmail.com>
2022-05-17 15:13:04 +0530

    test pipeline

--- tests/testing_data/prescript.py
+++ tests/testing_data/prescript.py
@@ -1,6 +1,11 @@
+import os
+
+import loguru
 import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 model_name = 'tuner007/pegasus_paraphrase'
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+loguru.logger.info("started loading pegasus tokenizer")
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+loguru.logger.info(os.listdir('/home/runner/.cache/huggingface/transformers'))
