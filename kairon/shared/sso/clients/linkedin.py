b0e67ce1c0f249c66dcdd9b57c860deb25061d31
udit-pandey <pandey.udit867@gmail.com>
2022-05-30 20:47:43 +0530

    updated code with upgrade in fastapi-sso package version

--- kairon/shared/sso/clients/linkedin.py
+++ kairon/shared/sso/clients/linkedin.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 
 import httpx
 from fastapi_sso.sso.base import OpenID
@@ -59,7 +59,9 @@ class LinkedinSSO(KaironSSO):
             "useremail_endpoint": f"{cls.profile_url}/emailAddress?q=members&projection=(elements*(handle~))"
         }
 
-    async def process_login(self, code: str, request: Request) -> Optional[OpenID]:
+    async def process_login(
+            self, code: str, request: Request, *, params: Optional[Dict[str, Any]] = None
+    ) -> Optional[OpenID]:
         """
         This method should be called from callback endpoint to verify the user and request user info endpoint.
         This is low level, you should use {verify_and_process} instead.
