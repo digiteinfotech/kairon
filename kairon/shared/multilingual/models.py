d8fc50e34b5479f6dd43d1076a57c3d6818b097f
siddhant_bane <siddhant.bane@digite.com>
2022-06-29 18:33:58 +0530

    Developed multilingual translation feature

---
+++ kairon/shared/multilingual/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, validator
+
+class TranslationRequest(BaseModel):
+    d_lang: str
+    translate_responses: bool = True
+    translate_actions: bool = False
+
+    @validator("d_lang")
+    def validate_synonym(cls, f, values, **kwargs):
+        from kairon.shared.utils import Utility
+
+        if Utility.check_empty_string(f):
+            raise ValueError("d_lang cannot be empty")
+        return f
