d082e89aa575e09549c2c0ab3eee82afa360c795
siddhant_bane <siddhant.bane@digite.com>
2022-07-12 19:48:44 +0530

    Added service tests

--- kairon/shared/multilingual/models.py
+++ kairon/shared/multilingual/models.py
@@ -8,7 +8,7 @@ class TranslationRequest(BaseModel):
 
     @classmethod
     @validator("d_lang")
-    def validate_synonym(cls, f, values, **kwargs):
+    def validate_d_lang(cls, f, values, **kwargs):
         from kairon.shared.utils import Utility
 
         if Utility.check_empty_string(f):
