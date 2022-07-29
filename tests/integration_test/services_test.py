bcb64a5a14b8bdeddf70f8f67a3b9bad449c24f4
Nupur Khare <78532321+nupur-khare@users.noreply.github.com>
2022-07-29 19:18:00 +0530

    Unable to train Rule only bot (#643)
    * 1. Bug-fix: Unable to train Rule only bot
    Solution: Added rule_count as part of validation
    2. Added unit tests.
    
    * 1. Fixed test failures.

--- tests/integration_test/services_test.py
+++ tests/integration_test/services_test.py
@@ -3376,7 +3376,7 @@ def test_train_insufficient_data(monkeypatch):
     assert not actual["success"]
     assert actual["error_code"] == 422
     assert actual["data"] is None
-    assert actual["message"] == "Please add at least 2 stories and 2 intents before training the bot!"
+    assert actual["message"] == "Please add at least 2 flows and 2 intents before training the bot!"
 
 
 def test_delete_bot():
