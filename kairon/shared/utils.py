0fc094f9d0d26add507e6a05e670d31a1ca81d52
Nupur Khare <78532321+nupur-khare@users.noreply.github.com>
2022-09-28 20:40:33 +0530

    1. Changes to retain latest last 4 models and delete the rest. Added unit and integration tests.

--- kairon/shared/utils.py
+++ kairon/shared/utils.py
@@ -1172,6 +1172,24 @@ class Utility:
         return Utility.get_latest_file(model_file, "*.tar.gz")
 
     @staticmethod
+    def delete_models(bot: Text, retain_cnt: int = 4):
+        """
+        fetches the latest model from the path
+
+        :param bot: bot id
+        :param retain_cnt: number of models
+        :return: latest model path
+        """
+        from rasa.shared.constants import DEFAULT_MODELS_PATH
+
+        model_file = os.path.join(DEFAULT_MODELS_PATH, bot, "old_model", "*.tar.gz")
+        file_list = glob(model_file)
+        file_list.sort(key=os.path.getctime, reverse=True)
+        for file in file_list[retain_cnt:]:
+            os.remove(file)
+        return file_list
+
+    @staticmethod
     def is_model_file_exists(bot: Text, raise_exc: bool = True):
         try:
             Utility.get_latest_model(bot)
