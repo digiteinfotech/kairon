99f0620399ec98eaaee343d63da07d1f170015a5
nupur_khare <nupur15june@gmail.com>
2023-02-01 10:57:50 +0530

    Removed unused imports.

--- kairon/shared/data/data_objects.py
+++ kairon/shared/data/data_objects.py
@@ -29,10 +29,9 @@ from rasa.shared.core.slots import (
 from validators import url, ValidationFailure
 from kairon.shared.data.signals import push_notification, auditlogger
 from kairon.exceptions import AppException
-from kairon.shared.utils import Utility, StoryValidator
+from kairon.shared.utils import Utility
 from kairon.shared.models import TemplateType, StoryStepType
 from validators import domain
-from mongoengine import signals
 
 
 class Entity(EmbeddedDocument):
