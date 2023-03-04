101d2f1531481fffd1ce6db96936f41eb7931239
fshaikh <fshaikh@digite.com>
2023-03-04 13:11:52 +0530

    gpt3 test cases fixed using rest api

--- kairon/shared/llm/gpt3.py
+++ kairon/shared/llm/gpt3.py
@@ -4,6 +4,8 @@ from kairon.shared.utils import Utility
 import openai
 from kairon.shared.data.data_objects import BotContent
 from urllib.parse import urljoin
+from kairon.exceptions import AppException
+from loguru import logger as logging
 
 
 class GPT3FAQEmbedding(LLMBase):
@@ -63,20 +65,24 @@ class GPT3FAQEmbedding(LLMBase):
         Utility.execute_http_request(http_url=urljoin(self.db_url, f"/collections/{collection_name}"),
                                                 request_method="DELETE",
                                                 headers=self.headers,
-                                                return_json=True)
+                                                return_json=False)
 
         Utility.execute_http_request(http_url=urljoin(self.db_url, f"/collections/{collection_name}"),
                                      request_method="PUT",
                                      headers=self.headers,
                                      request_body={'name': collection_name, 'vectors': self.vector_config},
-                                     return_json=True)
+                                     return_json=False)
 
     def __collection_upsert__(self, collection_name: Text, data: Union[List, Dict]):
-        Utility.execute_http_request(http_url=urljoin(self.db_url, f"/collections/{collection_name}/points"),
+        response = Utility.execute_http_request(http_url=urljoin(self.db_url, f"/collections/{collection_name}/points"),
                                      request_method="PUT",
                                      headers=self.headers,
                                      request_body=data,
                                      return_json=True)
+        if not response.get('result'):
+            if "status" in response:
+                logging.exception(response['status'].get('error'))
+                raise AppException("Unable to train faq! contact support")
 
     def __collection_search__(self, collection_name: Text, vector: List):
         response = Utility.execute_http_request(http_url=urljoin(self.db_url, f"/collections/{collection_name}/points/search"),
--- tests/unit_test/llm_test.py
+++ tests/unit_test/llm_test.py
@@ -1,14 +1,19 @@
-import pytest
 import os
-from kairon.shared.utils import Utility
-from mongoengine import connect
+from urllib.parse import urljoin
+
 import mock
-from kairon.shared.llm.gpt3 import GPT3FAQEmbedding, LLMBase
-from kairon.shared.llm.factory import LLMFactory
-from kairon.shared.data.data_objects import BotContent
 import numpy as np
-from openai.util import convert_to_openai_object
+import pytest
+import responses
+from mongoengine import connect
 from openai.openai_response import OpenAIResponse
+from openai.util import convert_to_openai_object
+
+from kairon.shared.data.data_objects import BotContent
+from kairon.shared.llm.factory import LLMFactory
+from kairon.shared.llm.gpt3 import GPT3FAQEmbedding, LLMBase
+from kairon.shared.utils import Utility
+from kairon.exceptions import AppException
 
 
 class TestLLM:
@@ -20,12 +25,12 @@ class TestLLM:
 
     def test_llm_base_train(self):
         with pytest.raises(Exception):
-            base = LLMBase()
+            base = LLMBase("Test")
             base.train()
 
     def test_llm_base_predict(self):
         with pytest.raises(Exception):
-            base = LLMBase()
+            base = LLMBase('Test')
             base.predict("Sample")
 
     def test_llm_factory_invalid_type(self):
@@ -45,51 +50,99 @@ class TestLLM:
             assert inst.db_url == Utility.environment['vector']['db']
             assert inst.headers == {'api-key': Utility.environment['vector']['key']}
 
+    @responses.activate
     @mock.patch("kairon.shared.llm.gpt3.openai.Embedding.create", autospec=True)
-    @mock.patch("kairon.shared.llm.gpt3.Utility.execute_http_request", autospec=True)
-    def test_gpt3_faq_embedding_train_collection_exists(self, mock_vec_request, mock_openai):
+    def test_gpt3_faq_embedding_train(self, mock_openai):
+        test_content = BotContent(
+            data="Welcome! Are you completely new to programming? If not then we presume you will be looking for information about why and how to get started with Python",
+            bot="test_embed_faq", user="test").save()
+
         embedding = list(np.random.random(GPT3FAQEmbedding.__embedding__))
-        mock_vec_request.side_effects = [{'result': {'status': 'green'}}, {}]
         mock_openai.return_value = convert_to_openai_object(OpenAIResponse({'data': [{'embedding': embedding}]}, {}))
-
         with mock.patch.dict(Utility.environment, {'llm': {"faq": "GPT3_FAQ_EMBED", 'api_key': 'test'}}):
-            test_content = BotContent(
-                data="Welcome! Are you completely new to programming? If not then we presume you will be looking for information about why and how to get started with Python",
-                bot="test_embed_faq", user="test").save()
-
             gpt3 = GPT3FAQEmbedding(test_content.bot)
-            response = gpt3.train()
 
+            responses.add(
+                "DELETE",
+                urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}"),
+                adding_headers={}
+            )
+
+            responses.add(
+                url=urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}"),
+                method="PUT",
+                adding_headers={},
+                match=[responses.json_params_matcher({'name': gpt3.bot + gpt3.suffix, 'vectors': gpt3.vector_config})],
+                status=200
+            )
+
+            responses.add(
+                url=urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}/points"),
+                method="PUT",
+                adding_headers={},
+                match=[responses.json_params_matcher({'points': [{'id': test_content.vector_id,
+                                                                  'vector': embedding,
+                                                                  'payload': {'content': test_content.data}
+                                                                  }]})],
+                json={"result": {"operation_id": 0, "status": "acknowledged"}, "status": "ok", "time": 0.003612634}
+            )
+
+            response = gpt3.train()
             assert response['faq'] == 1
 
             assert mock_openai.call_args.kwargs['api_key'] == Utility.environment['llm']['api_key']
             assert mock_openai.call_args.kwargs['input'] == test_content.data
 
+    @responses.activate
     @mock.patch("kairon.shared.llm.gpt3.openai.Embedding.create", autospec=True)
-    @mock.patch("kairon.shared.llm.gpt3.Utility.execute_http_request", autospec=True)
-    def test_gpt3_faq_embedding_train_collection_does_not_exists(self,
-                                                                 mock_vec_request,
-                                                                 mock_openai):
+    def test_gpt3_faq_embedding_train_upsert_error(self, mock_openai):
+        test_content = BotContent(
+            data="Welcome! Are you completely new to programming? If not then we presume you will be looking for information about why and how to get started with Python",
+            bot="test_embed_faq_not_exists", user="test").save()
+
         embedding = list(np.random.random(GPT3FAQEmbedding.__embedding__))
         mock_openai.return_value = convert_to_openai_object(OpenAIResponse({'data': [{'embedding': embedding}]}, {}))
 
         with mock.patch.dict(Utility.environment, {'llm': {"faq": "GPT3_FAQ_EMBED", 'api_key': 'test'}}):
-            test_content = BotContent(
-                data="Welcome! Are you completely new to programming? If not then we presume you will be looking for information about why and how to get started with Python",
-                bot="test_embed_faq_not_exists", user="test").save()
-
             gpt3 = GPT3FAQEmbedding(test_content.bot)
-            response = gpt3.train()
-            assert response['faq'] == 1
 
-            assert mock_openai.call_args.kwargs['api_key'] == Utility.environment['llm']['api_key']
-            assert mock_openai.call_args.kwargs['input'] == test_content.data
+            responses.add(
+                "DELETE",
+                urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}"),
+                adding_headers={}
+            )
 
+            responses.add(
+                url=urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}"),
+                method="PUT",
+                adding_headers={},
+                match=[responses.json_params_matcher({'name': gpt3.bot + gpt3.suffix, 'vectors': gpt3.vector_config})],
+                status=200
+            )
+
+            responses.add(
+                url=urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}/points"),
+                method="PUT",
+                adding_headers={},
+                match=[responses.json_params_matcher({'points': [{'id': test_content.vector_id,
+                                                                  'vector': embedding,
+                                                                  'payload': {'content': test_content.data}
+                                                                  }]})],
+                json={"result": None,
+                      'status': {'error': 'Json deserialize error: missing field `vectors` at line 1 column 34779'},
+                      "time": 0.003612634}
+            )
+
+            with pytest.raises(AppException, match="Unable to train faq! contact support"):
+                gpt3.train()
+
+                assert mock_openai.call_args.kwargs['api_key'] == Utility.environment['llm']['api_key']
+                assert mock_openai.call_args.kwargs['input'] == test_content.data
+
+    @responses.activate
     @mock.patch("kairon.shared.llm.gpt3.openai.Completion.create", autospec=True)
     @mock.patch("kairon.shared.llm.gpt3.openai.Embedding.create", autospec=True)
-    @mock.patch("kairon.shared.llm.gpt3.Utility.execute_http_request", autospec=True)
     def test_gpt3_faq_embedding_predict(self,
-                                        mock_vec_client,
                                         mock_embedding,
                                         mock_completion
                                         ):
@@ -103,12 +156,22 @@ class TestLLM:
         query = "What kind of language is python?"
 
         mock_embedding.return_value = convert_to_openai_object(OpenAIResponse({'data': [{'embedding': embedding}]}, {}))
-        mock_completion.return_value = convert_to_openai_object(OpenAIResponse({'choices': [{'text': generated_text}]}, {}))
-        mock_vec_client.return_value = {'result': [{'id': test_content.id.__str__(), 'score':0.80, "payload":{'content': test_content.data}}]}
+        mock_completion.return_value = convert_to_openai_object(
+            OpenAIResponse({'choices': [{'text': generated_text}]}, {}))
 
         with mock.patch.dict(Utility.environment, {'llm': {"faq": "GPT3_FAQ_EMBED", 'api_key': 'test'}}):
-
             gpt3 = GPT3FAQEmbedding(test_content.bot)
+
+            responses.add(
+                url=urljoin(Utility.environment['vector']['db'], f"/collections/{gpt3.bot}{gpt3.suffix}/points/search"),
+                method="POST",
+                adding_headers={},
+                match=[responses.json_params_matcher(
+                    {'vector': embedding, 'limit': 10, 'with_payload': True, 'score_threshold': 0.70})],
+                json={'result': [
+                    {'id': test_content.vector_id, 'score': 0.80, "payload": {'content': test_content.data}}]}
+            )
+
             response = gpt3.predict(query)
 
             assert response['content'] == generated_text
@@ -117,5 +180,7 @@ class TestLLM:
             assert mock_embedding.call_args.kwargs['input'] == query
 
             assert mock_completion.call_args.kwargs['api_key'] == Utility.environment['llm']['api_key']
-            assert mock_completion.call_args.kwargs['prompt'] == f"{gpt3.__answer_command__} \n\nContext:\n{test_content.data}\n\n Q: {query}\n A:"
-            assert all(mock_completion.call_args.kwargs[key] == gpt3.__answer_params__[key] for key in gpt3.__answer_params__.keys())
+            assert mock_completion.call_args.kwargs[
+                       'prompt'] == f"{gpt3.__answer_command__} \n\nContext:\n{test_content.data}\n\n Q: {query}\n A:"
+            assert all(mock_completion.call_args.kwargs[key] == gpt3.__answer_params__[key] for key in
+                       gpt3.__answer_params__.keys())
