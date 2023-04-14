f93cb7cf390949acdba84deb9df910ac443b73b2
nupur_khare <nupur15june@gmail.com>
2023-04-14 15:42:21 +0530

    1. added a flattened conversation event while pushing conversation events in tracker 2. Optimised flattened data  aggregation pipeline which failed due to unordered insertion of conversation events in tracker 3. unit and integration tests

--- kairon/history/processor.py
+++ kairon/history/processor.py
@@ -831,10 +831,10 @@ class HistoryProcessor:
                                           "confidence": "$data.confidence", "action": "$data.action",
                                           "timestamp": {"$toDate": {"$multiply": ["$timestamp", 1000]}},
                                           "bot_response_text": "$data.bot_response_text",
-                                          "bot_response_data": "$data.bot_response_data",
+                                          "bot_response_data": "$data.bot_response_data", "sender_id": "$sender_id"
                                           }},
                             {"$sort": {"timestamp": -1}},
-                            {"$project": {"user_input": 1, "intent": 1, "confidence": 1, "action": 1,
+                            {"$project": {"_id": 0, "user_input": 1, "intent": 1, "confidence": 1, "action": 1,
                                           "timestamp": {'$dateToString': {'format': "%d-%m-%Y %H:%M:%S", 'date': '$timestamp'}},
                                           "bot_response_text": 1, "bot_response_data": 1}}
                          ], allowDiskUse=True))
