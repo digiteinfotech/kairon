77c8adaf11f3561b3fade497143b4d62fc8795db
Nupur Khare <nupur.khare@digite.com>
2023-05-11 13:46:53 +0530

    Fixed test cases.

--- tests/integration_test/action_service_test.py
+++ tests/integration_test/action_service_test.py
@@ -6571,22 +6571,13 @@ class TestActionServer(AsyncHTTPTestCase):
         log = ActionServerLogs.objects(bot=bot, type=ActionType.kairon_faq_action.value, status="SUCCESS").get()
         assert log['llm_logs'] == [{'message': 'Response added to cache', 'type': 'response_cached'}]
 
-    @patch("kairon.shared.llm.gpt3.openai.ChatCompletion.create", autospec=True)
-    @patch("kairon.shared.llm.gpt3.openai.Embedding.create", autospec=True)
     @patch("kairon.shared.llm.gpt3.Utility.execute_http_request", autospec=True)
-    def test_kairon_faq_response_action_with_action_not_found(self, mock_search, mock_embedding, mock_completion):
-        from kairon.shared.llm.gpt3 import GPT3FAQEmbedding
-        from openai.util import convert_to_openai_object
-        from openai.openai_response import OpenAIResponse
-        from uuid6 import uuid7
-
+    def test_kairon_faq_response_action_with_action_not_found(self, mock_search):
         action_name = KAIRON_FAQ_ACTION
         bot = "5u08kd0a00b698ca10d98u9q"
         user = "nupurk"
         value = "keyvalue"
         user_msg = "What kind of language is python?"
-        bot_content = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Python is dynamically typed and garbage-collected."
-        generated_text = "Python is dynamically typed, garbage-collected, high level, general purpose programming."
         llm_prompts = [
             {'name': 'System Prompt', 'data': 'You are a personal assistant.',
              'instructions': 'Answer question based on the context below.', 'type': 'system', 'source': 'static',
@@ -6598,40 +6589,12 @@ class TestActionServer(AsyncHTTPTestCase):
              'data': 'A programming language is a system of notation for writing computer programs.[1] Most programming languages are text-based formal languages, but they may also be graphical. They are a kind of computer language.',
              'instructions': 'Answer according to the context', 'type': 'user', 'source': 'static',
              'is_enabled': True},
-            {'name': 'Java Prompt',
-             'data': 'Java is a programming language and computing platform first released by Sun Microsystems in 1995.',
-             'instructions': 'Answer according to the context', 'type': 'user', 'source': 'static',
-             'is_enabled': True},
-            {'name': 'Slot Prompt',
-             'data': 'name',
-             'instructions': 'Answer according to the context', 'type': 'user', 'source': 'slot',
-             'is_enabled': True},
             {'name': 'Action Prompt',
              'data': 'http_action',
              'instructions': 'Answer according to the context', 'type': 'user', 'source': 'action',
              'is_enabled': True}
         ]
 
-        def mock_completion_for_answer(*args, **kwargs):
-            return convert_to_openai_object(
-                OpenAIResponse({'choices': [{'message': {'content': generated_text, 'role': 'assistant'}}]}, {}))
-
-        def __mock_search_cache(*args, **kwargs):
-            return {'result': []}
-
-        def __mock_fetch_similar(*args, **kwargs):
-            return {'result': [{'id': uuid7().__str__(), 'score': 0.80, 'payload': {'content': bot_content}}]}
-
-        def __mock_cache_result(*args, **kwargs):
-            return {'result': []}
-
-        mock_completion.return_value = mock_completion_for_answer()
-
-        embedding = list(np.random.random(GPT3FAQEmbedding.__embedding__))
-        mock_embedding.return_value = convert_to_openai_object(OpenAIResponse({'data': [{'embedding': embedding}]}, {}))
-        mock_completion.return_value = convert_to_openai_object(
-            OpenAIResponse({'choices': [{'message': {'content': generated_text, 'role': 'assistant'}}]}, {}))
-        mock_search.side_effect = [__mock_search_cache(), __mock_fetch_similar(), __mock_cache_result()]
         Actions(name=action_name, type=ActionType.kairon_faq_action.value, bot=bot, user=user).save()
         BotSettings(enable_gpt_llm_faq=True, bot=bot, user=user).save()
         KaironFaqAction(bot=bot, user=user, llm_prompts=llm_prompts).save()
@@ -6642,16 +6605,14 @@ class TestActionServer(AsyncHTTPTestCase):
         request_object["next_action"] = action_name
         request_object["tracker"]["sender_id"] = user
         request_object["tracker"]["latest_message"]['text'] = user_msg
-        request_object['tracker']['events'] = [{"event": "bot", 'text': 'hello'},
-                                               {'event': 'bot', "text": "how are you"}]
 
         response = self.fetch("/webhook", method="POST", body=json.dumps(request_object).encode('utf-8'))
         response_json = json.loads(response.body.decode("utf8"))
         self.assertEqual(response_json['events'], [
-            {'event': 'slot', 'timestamp': None, 'name': 'kairon_action_response', 'value': 'Faq feature is disabled for the bot! Please contact support.'}])
+            {'event': 'slot', 'timestamp': None, 'name': 'kairon_action_response', 'value': "I'm sorry, I didn't quite understand that. Could you rephrase?"}])
         self.assertEqual(
             response_json['responses'],
-            [{'text': 'Faq feature is disabled for the bot! Please contact support.', 'buttons': [], 'elements': [],
+            [{'text': "I'm sorry, I didn't quite understand that. Could you rephrase?", 'buttons': [], 'elements': [],
               'custom': {}, 'template': None, 'response': None, 'image': None, 'attachment': None}]
         )
         log = ActionServerLogs.objects(bot=bot, type=ActionType.kairon_faq_action.value, status="FAILURE").get()
