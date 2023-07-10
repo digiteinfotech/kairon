261471e9069ce5a9f6c2f141914eb7ad7182bd42
Mahesh <maheshsattala@gmail.com>
2023-07-10 18:36:38 +0530

    Modified whatsapp file and Added on_premise and test cases in channel_client_test.

---
+++ tests/unit_test/channel_client_test.py
@@ -0,0 +1,60 @@
+import pytest
+import unittest.mock as mock
+
+from kairon.chat.handlers.channels.clients.whatsapp.on_premise import WhatsappOnPremise
+from unittest.mock import patch
+
+
+class TestWhatsappOnPremise:
+
+    @pytest.fixture(scope="module")
+    def whatsapp_on_premise(self):
+        access_token = "ERTYUIEFDGHGFHJKLFGHJKGHJ"
+        from_phone_number_id = "918958030415"
+        whatsapp_on_premise = WhatsappOnPremise(access_token=access_token, from_phone_number_id=from_phone_number_id)
+        yield whatsapp_on_premise
+
+    def test_send_action(self, whatsapp_on_premise):
+        with mock.patch.object(whatsapp_on_premise.session, 'post') as mock_post:
+            mock_post.return_value.json.return_value = {"messages": [{"id": "test_id"}]}
+            response = whatsapp_on_premise.send_action(payload={"text": "Hi"})
+            mock_post.assert_called_once_with(
+                'https://graph.facebook.com/v13.0/messages',
+                headers=whatsapp_on_premise.auth_args,
+                json={'text': 'Hi'}, timeout=None
+            )
+            assert response == {"messages": [{"id": "test_id"}]}
+
+    def test_get_attachment(self, whatsapp_on_premise):
+        with mock.patch.object(whatsapp_on_premise.session, 'get') as mock_get:
+            mock_get.return_value.json.return_value = {"type": "document", "media_id": "test_media_id"}
+            response = whatsapp_on_premise.get_attachment(media_id="test_media_id")
+            mock_get.assert_called_once_with(
+                'https://graph.facebook.com/v13.0/media/test_media_id',
+                headers=whatsapp_on_premise.auth_args,
+                timeout=None
+            )
+            assert response == {"type": "document", "media_id": "test_media_id"}
+
+    def test_mark_as_read(self, whatsapp_on_premise):
+        with mock.patch.object(whatsapp_on_premise.session, 'put') as mock_put:
+            mock_put.return_value.json.return_value = {"id": "test_msg_id"}
+            response = whatsapp_on_premise.mark_as_read(msg_id="test_msg_id")
+            mock_put.assert_called_once_with(
+                'https://graph.facebook.com/v13.0/messages/test_msg_id',
+                headers=whatsapp_on_premise.auth_args,
+                json={'status': 'read'}, timeout=None
+            )
+            assert response == {"id": "test_msg_id"}
+
+    def test_send_template_message(self, whatsapp_on_premise):
+        namespace = "test_namespace"
+        name = "test_template_name"
+        to_phone_number = "9876543210"
+        with patch("kairon.chat.handlers.channels.clients.whatsapp.on_premise.WhatsappOnPremise.send",
+                   autospec=True) as mock_send:
+            mock_send.return_value = {
+                "contacts": [{"input": "+55123456789", "status": "valid", "wa_id": "55123456789"}]}
+            response = whatsapp_on_premise.send_template_message(namespace=namespace, name=name,
+                                                                 to_phone_number=to_phone_number)
+            assert response == {"contacts": [{"input": "+55123456789", "status": "valid", "wa_id": "55123456789"}]}
