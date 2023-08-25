fc78c59c31cf10c94d3f69ff5c6930aae8f56642
Mahesh <maheshsattala@gmail.com>
2023-08-25 12:52:56 +0530

    handled AppException in evaluate_script

--- kairon/evaluator/processor.py
+++ kairon/evaluator/processor.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
+from kairon.exceptions import AppException
 from kairon.shared.concurrency.orchestrator import ActorOrchestrator
 from kairon.shared.constants import ActorType
 
@@ -12,7 +13,7 @@ class EvaluatorProcessor:
         response = None
         try:
             response = ActorOrchestrator.run(ActorType.pyscript_runner.value, source_code=source_code)
-        except Exception as e:
-            logger.error(e)
-            message = str(e)
+        except AppException as e:
+            message = f"Failed to evaluate the script: {str(e)}"
+            logger.error(message)
         return response, message
