c3f86d959b5cf0df2e7926fb536c34d4f054cd1e
Fahad Ali Shaikh <fshaikh@digite.com>
2023-09-05 12:48:38 +0530

    fixed setupfiles

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 python -m pip install -r requirements/dev.txt
 python -m spacy download en_core_web_md
 python -m spacy link en_core_web_md en
-python -m nltk.downloader wordnet
 python -m nltk.downloader averaged_perceptron_tagger
 python -m nltk.downloader punkt
 python -m nltk.downloader stopwords
 python -m nltk.downloader omw-1.4
+python -m nltk.downloader wordnet
