1c2fbe62ac2bbcde557b7a73f3ea50f6fd91147a
Fahad Ali Shaikh <fshaikh@digite.com>
2023-09-05 14:21:49 +0530

    python version in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python -m pip install -r requirements/dev.txt
-python -m spacy download en_core_web_md
-python -m spacy link en_core_web_md en
-python -m nltk.downloader averaged_perceptron_tagger
-python -m nltk.downloader punkt
-python -m nltk.downloader stopwords
-python -m nltk.downloader omw-1.4
-python -m nltk.downloader wordnet
+python3.10 -m pip install -r requirements/dev.txt
+python3.10 -m spacy download en_core_web_md
+python3.10 -m spacy link en_core_web_md en
+python3.10 -m nltk.downloader averaged_perceptron_tagger
+python3.10 -m nltk.downloader punkt
+python3.10 -m nltk.downloader stopwords
+python3.10 -m nltk.downloader omw-1.4
+python3.10 -m nltk.downloader wordnet
