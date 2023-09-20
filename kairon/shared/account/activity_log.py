7c40462e54d86161d4dc57c7a5209e3cb1acdc31
Nupur Khare <nupur.khare@digite.com>
2023-09-20 20:24:33 +0530

    1. Limited login attempts to 3. 2. UserActivityLog logs will be added to AuditLogData. 3. Added unit and integration test cases. 4. Fixed unit and integration test cases.

--- kairon/shared/account/activity_log.py
+++ kairon/shared/account/activity_log.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from typing import Text
 
 from kairon.exceptions import AppException
+from kairon.shared.account.data_objects import User
 from kairon.shared.constants import UserActivityType
 from kairon.shared.data.base_data import AuditLogData
 from kairon.shared.data.constant import AuditlogActions
@@ -25,10 +26,12 @@ class UserActivityLogger:
     @staticmethod
     def is_password_reset_request_limit_exceeded(email: Text):
         reset_password_request_limit = Utility.environment['user']['reset_password_request_limit']
-        reset_password_request_count = AuditLogData.objects(
-            user=email, action=AuditlogActions.ACTIVITY.value, entity=UserActivityType.reset_password_request.value,
-            timestamp__gte=datetime.utcnow().date()
-        ).exclude('audit').count()
+        user = User.objects(email__iexact=email, status=True).get()
+        reset_password_request_count = AuditLogData.objects(audit={'Bot_id': 'None', 'account': user['account']},
+                                                            user=email, action=AuditlogActions.ACTIVITY.value,
+                                                            entity=UserActivityType.reset_password_request.value,
+                                                            timestamp__gte=datetime.utcnow().date()
+                                                            ).count()
         if reset_password_request_count >= reset_password_request_limit:
             raise AppException('Password reset limit exhausted for today.')
 
@@ -36,13 +39,15 @@ class UserActivityLogger:
     def is_password_reset_within_cooldown_period(email: Text):
         reset_password_request_limit = Utility.environment['user']['reset_password_cooldown_period']
         cooldown_period = datetime.utcnow().__sub__(timedelta(seconds=reset_password_request_limit * 60))
+        user = User.objects(email__iexact=email, status=True).get()
         if Utility.is_exist(
                 AuditLogData, raise_error=False, user=email, action=AuditlogActions.ACTIVITY.value, entity=UserActivityType.reset_password.value,
                 timestamp__gte=cooldown_period, check_base_fields=False
         ):
-            log = AuditLogData.objects(
-                user=email, action=AuditlogActions.ACTIVITY.value, entity=UserActivityType.reset_password.value, timestamp__gte=cooldown_period
-            ).exclude('audit').get()
+            log = AuditLogData.objects(audit={'Bot_id': 'None', 'account': user['account']},
+                                       user=email, action=AuditlogActions.ACTIVITY.value,
+                                       entity=UserActivityType.reset_password.value,
+                                       timestamp__gte=cooldown_period).get()
             raise AppException(
                 f'Password reset limit exhausted. Please come back in '
                 f'{str(timedelta(seconds=(datetime.utcnow() - log.timestamp).seconds))}'
