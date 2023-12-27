3087a02849463ca83f44098486dfe332ae462b32
udit_pandey <pandey.udit867@gmail.com>
2023-12-27 10:19:41 +0530

    fixed test failure
    Co-authored-by: udit-pandey <pandey.udit867@gmail.com>
    Co-authored-by: nupur-khare <nupur15june@gmail.com>

--- tests/integration_test/services_test.py
+++ tests/integration_test/services_test.py
@@ -14098,9 +14098,7 @@ def test_get_channel_logs():
     print(actual)
     assert actual["success"]
     assert actual["error_code"] == 0
-    assert actual["data"] == [{'status': 'delivered', 'campaign_id': '6779002886649302', 'count': 1},
-                              {'status': 'read', 'campaign_id': '6779002886649302', 'count': 1},
-                              {'status': 'sent', 'campaign_id': '6779002886649302', 'count': 1}]
+    assert actual["data"] == [{'campaign_id': '6779002886649302', 'status': {'delivered': 1, 'read': 1, 'sent': 1}}]
 
 
 @responses.activate
