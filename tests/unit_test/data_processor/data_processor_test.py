a10c5bdf8f3f9307ef634379c0a838092e69dd2b
Fahad Ali Shaikh <fshaikh@digite.com>
2024-05-27 15:19:06 +0530

    internal fix

--- tests/unit_test/data_processor/data_processor_test.py
+++ tests/unit_test/data_processor/data_processor_test.py
@@ -15641,8 +15641,7 @@ class TestModelProcessor:
         start_training("tests", "testUser")
         model_training = ModelTraining.objects(bot="tests", status="Fail")
         assert model_training.__len__() == 2
-        print(model_training[1].to_mongo().to_dict())
-        assert model_training[1].exception in str("invalid model file")
+        assert model_training[1].exception in str("Failed to load the model for the bot.")
 
     def test_train_model_for_bot(self):
         model = train_model_for_bot("tests")
