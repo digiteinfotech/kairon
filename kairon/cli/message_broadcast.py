f90fce8cd532a8df2f61046b493c80830ac218a0
Mahesh <maheshsattala@gmail.com>
2024-07-29 13:28:46 +0530

    Added code related to cli for resend broadcast and added tests for the same.

--- kairon/cli/message_broadcast.py
+++ kairon/cli/message_broadcast.py
@@ -44,6 +44,7 @@ def add_subparser(subparsers: SubParsersAction, parents: List[ArgumentParser]):
                           help="Broadcast config document id or broadcast log reference id", action='store')
     notifier.add_argument('is_resend',
                           type=bool,
+                          default=False,
                           help="Specify if the broadcast is a resend (True) or a normal broadcast (False).",
                           action='store')
     notifier.set_defaults(func=send_notifications)
