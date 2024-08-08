f70229b9b4a4db69a8a8a367bdee7c8051b6da5a
Fahad Ali Shaikh <fshaikh@digite.com>
2024-08-08 16:09:37 +0530

    scheduler action test cases added

--- kairon/actions/definitions/schedule.py
+++ kairon/actions/definitions/schedule.py
@@ -14,6 +14,7 @@ from kairon.shared.actions.models import ActionType
 from kairon.shared.actions.utils import ActionUtility
 from kairon.shared.callback.data_objects import CallbackConfig
 from kairon.shared.constants import EventClass
+from dateutil import parser as date_parser
 
 
 class ActionSchedule(ActionsBase):
