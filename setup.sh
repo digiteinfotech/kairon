ed73f31908277124c19cf2a7336a7191ea17d435
hghuge <hghuge@digite.com>
2024-08-12 12:10:12 +0530

    - nltk downloader averaged_perceptron_tagger_eng

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 python -m pip install -r requirements/dev.txt
 python -c "import spacy; spacy.cli.download('en_core_web_md')"
 python -m spacy link en_core_web_md en
+python -c "import nltk;nltk.download('averaged_perceptron_tagger_eng')"
 python -c "import nltk;nltk.download('averaged_perceptron_tagger')"
 python -c "import nltk;nltk.download('punkt')"
 python -c "import nltk;nltk.download('stopwords')"
