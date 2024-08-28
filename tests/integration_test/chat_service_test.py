7407c21a399941b4af0c15de4a6fa6231617ea1c
Mahesh <maheshsattala@gmail.com>
2024-08-28 20:53:23 +0530

    Added code for handling the webhook event after completing payment and added test cases.

--- tests/integration_test/chat_service_test.py
+++ tests/integration_test/chat_service_test.py
@@ -2187,7 +2187,7 @@ def test_whatsapp_valid_attachment_message_request():
     responses.add("POST", "https://graph.facebook.com/v19.0/12345678/messages", json={})
     responses.add(
         "GET",
-        "https://graph.facebook.com/v13.0/sdfghj567",
+        "https://graph.facebook.com/v19.0/sdfghj567",
         json={
             "messaging_product": "whatsapp",
             "url": "http://kairon-media.url",
@@ -2273,7 +2273,7 @@ def test_whatsapp_payment_message_request():
     def _mock_validate_hub_signature(*args, **kwargs):
         return True
 
-    responses.add("POST", "https://graph.facebook.com/v13.0/12345678/messages", json={})
+    responses.add("POST", "https://graph.facebook.com/v19.0/12345678/messages", json={})
 
     with patch.object(
             MessengerHandler, "validate_hub_signature", _mock_validate_hub_signature
@@ -2911,7 +2911,7 @@ def test_whatsapp_valid_unsupported_message_request():
     def _mock_validate_hub_signature(*args, **kwargs):
         return True
 
-    responses.add("POST", "https://graph.facebook.com/v13.0/12345678/messages", json={})
+    responses.add("POST", "https://graph.facebook.com/v19.0/12345678/messages", json={})
 
     with patch.object(
             MessengerHandler, "validate_hub_signature", _mock_validate_hub_signature
