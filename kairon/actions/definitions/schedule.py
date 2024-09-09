3dd5c5dd1bc2cb8b2cf7db70c9f07c248e6ed5e7
Mahesh <maheshsattala@gmail.com>
2024-09-09 14:37:55 +0530

    Added code for executor logs and fixed respective test cases

--- kairon/actions/definitions/schedule.py
+++ kairon/actions/definitions/schedule.py
@@ -25,8 +25,6 @@ from kairon.shared.actions.exception import ActionFailure
 from kairon.shared.actions.models import ActionType
 from kairon.shared.actions.utils import ActionUtility
 from kairon.shared.callback.data_objects import CallbackConfig
-from kairon.shared.constants import EventClass
-from kairon.shared.data.constant import TASK_TYPE
 
 
 class ActionSchedule(ActionsBase):
