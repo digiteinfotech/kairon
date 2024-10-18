83b2141ed1e4be61407071cc00cf6f4560fa73c3
himanshu_gupta <himanshu.gupta@nimblework.com>
2024-10-18 14:08:12 +0530

    Updated delete api of cognition schema

--- kairon/api/app/routers/bot/data.py
+++ kairon/api/app/routers/bot/data.py
@@ -10,7 +10,7 @@ from kairon.events.definitions.content_importer import DocContentImporterEvent
 from kairon.events.definitions.faq_importer import FaqDataImporterEvent
 from kairon.shared.auth import Authentication
 from kairon.shared.cognition.processor import CognitionDataProcessor
-from kairon.shared.constants import DESIGNER_ACCESS
+from kairon.shared.constants import DESIGNER_ACCESS, ActorType
 from kairon.shared.data.processor import MongoProcessor
 from kairon.shared.models import User
 from kairon.shared.utils import Utility
@@ -83,9 +83,12 @@ async def delete_cognition_schema(
     """
     Deletes cognition content of the bot
     """
-    cognition_processor.delete_cognition_schema(schema_id, current_user.get_bot(), user=current_user.get_user())
+    from kairon.shared.concurrency.actors.factory import ActorFactory
+    actor = ActorFactory.get_instance(ActorType.callable_runner.value)
+    actor.execute(cognition_processor.delete_cognition_schema, schema_id, current_user.get_bot(),
+                  user=current_user.get_user())
     return {
-        "message": "Schema deleted!"
+       "message": "Schema will be deleted soon!"
     }
 
 
