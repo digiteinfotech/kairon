a4ec979820f08379de9cb73fd533a92bbfb0be1d
fahad_shaikh <fshaikh@digite.com>
2025-01-16 12:53:28 +0530

    link command removed spacy

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python -m pip install -r requirements/dev.txt
 python -c "import spacy; spacy.cli.download('en_core_web_md')"
-python -m spacy link en_core_web_md en
 python -c "import nltk;nltk.download('averaged_perceptron_tagger_eng')"
 python -c "import nltk;nltk.download('averaged_perceptron_tagger')"
 python -c "import nltk;nltk.download('punkt')"
