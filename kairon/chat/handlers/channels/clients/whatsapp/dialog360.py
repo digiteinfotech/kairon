a6ae2a1431eef80efa48397c9aea5b342fd9e5c9
spandan_mondal <spandan.mondal@nimblework.com>
2025-01-23 20:14:15 +0530

    added missing await

--- kairon/chat/handlers/channels/clients/whatsapp/dialog360.py
+++ kairon/chat/handlers/channels/clients/whatsapp/dialog360.py
@@ -67,9 +67,9 @@ class BSP360Dialog(WhatsappCloud):
                         return True, response.status, resp
                     else:
                         try:
-                            last_response = response.json()
+                            last_response = await response.json()
                         except Exception:
-                            last_response = response.text
+                            last_response = await response.text()
 
                 return False, last_status_code, last_response
         except ClientResponseError as cre:
