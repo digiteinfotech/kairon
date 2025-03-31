d3a811ddc3e5655f5f542797069ab8ade97a3b6d
spandan_mondal <spandan.mondal@nimblework.com>
2025-03-31 15:30:28 +0530

    flow tag in import and export

--- tests/unit_test/channels/mail_channel_test.py
+++ tests/unit_test/channels/mail_channel_test.py
@@ -29,7 +29,6 @@ bot_data_created = False
 class TestMailChannel:
     @pytest.fixture(autouse=True, scope='class')
     def setup(self):
-        global bot_data_created
         connect(**Utility.mongoengine_connection(Utility.environment['database']["url"]))
         a = Account.objects.create(name="mail_channel_test_user_acc", user="mail_channel_test_user_acc")
         bot = Bot.objects.create(name="mail_channel_test_bot", user="mail_channel_test_user_acc", status=True,
