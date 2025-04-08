2f26b11b96fccbfb403f8de0abc614b96633bde2
GursagarSandhu <sandhusagar2002@gmail.com>
2025-04-08 18:48:26 +0530

    special char issue on demo bot_name, update_story, add_story, user_names book a demo issue

--- kairon/shared/utils.py
+++ kairon/shared/utils.py
@@ -2213,22 +2213,6 @@ class Utility:
         if history_prompt_count > 1:
             raise exception_class("Only one history source can be present!")
 
-
-    @staticmethod
-    def validate_data(form_data):
-        """
-        Validates the first name and last name of the user, while booking demo.
-        :param form_data: Form data containing first name and last name
-        :return: None
-        """
-        first_name = form_data.data.get("first_name")
-        last_name = form_data.data.get("last_name")
-
-        if not Utility.special_match(first_name, search=RE_ALPHA_NUM):
-            raise AppException("First name can only contain letters, numbers, spaces and underscores.")
-        if not Utility.special_match(last_name, search=RE_ALPHA_NUM):
-            raise AppException("Last name can only contain letters, numbers, spaces and underscores.")
-
     @staticmethod
     def validate_data(form_data):
         """
