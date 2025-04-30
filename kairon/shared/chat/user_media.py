97eacdc77b097521ecfc3cb0c29f94bf6d3bf615
spandan_mondal <spandan.mondal@nimblework.com>
2025-04-30 17:14:24 +0530

    whataspp media upload support

--- kairon/shared/chat/user_media.py
+++ kairon/shared/chat/user_media.py
@@ -1,5 +1,6 @@
 import asyncio
 import base64
+import mimetypes
 import os
 from datetime import datetime
 from pathlib import Path
@@ -7,6 +8,7 @@ from typing import BinaryIO
 from markdown_pdf import MarkdownPdf, Section
 from loguru import logger
 from fastapi import File
+import requests
 from mongoengine import DoesNotExist
 from pathy import ClientError
 from uuid6 import uuid7
@@ -23,6 +25,10 @@ class UserMedia:
 
     @staticmethod
     def create_user_media_data(bot: str, media_id: str, filename: str, sender_id: str, upload_type: str = UserMediaUploadType.user_uploaded.value):
+        """
+        Create user media data in processing state.
+        Call mark_user_media_data_upload_done() to mark the upload as done or mark_user_media_data_upload_failed() to mark the upload as failed.
+        """
         extension = str(Path(filename).suffix).lower()
         user_media_data = UserMediaData(
             media_id=media_id,
@@ -38,6 +44,9 @@ class UserMedia:
 
     @staticmethod
     def mark_user_media_data_upload_done(media_id:str, media_url: str, output_filename: str, filesize: int):
+        """
+        Mark user media data upload as done.
+        """
         user_media_data = UserMediaData.objects(media_id=media_id).first()
         if user_media_data:
             user_media_data.media_url = media_url
@@ -64,6 +73,14 @@ class UserMedia:
                 binary_data: bytes = None,
                 filename: str = None,
                 ):
+        """
+        Save media content to cloud storage.
+        :param bot: bot name
+        :param sender_id: sender id
+        :param media_id: media id
+        :param binary_data: binary data of the file
+        :param filename: name of the file
+        """
 
         if not filename:
             raise AppException('filename must be provided for binary data')
@@ -100,6 +117,84 @@ class UserMedia:
             raise AppException(f"File upload for {media_id} failed")
 
     @staticmethod
+    def save_whatsapp_media_content(bot: str, sender_id: str, whatsapp_media_id:str, config: dict):
+        """
+        Download media from 360 dialog or meta and save it to cloud storage via background task.
+        :param bot: bot name
+        :param sender_id: sender id
+        :param whatsapp_media_id: whatsapp media id
+        :param config: configuration for 360 dialog or meta
+        :return: list of media ids
+        """
+        download_url = None
+        file_path = None
+        headers = {}
+        provider = config.get("bsp_type", "meta")
+        if provider == '360dialog':
+            endpoint = f'https://waba-v2.360dialog.io/{whatsapp_media_id}'
+            headers = {
+                'D360-API-KEY': config.get('api_key'),
+            }
+            resp = requests.get(endpoint, headers=headers, stream=True)
+            if resp.status_code != 200:
+                raise AppException(f"Failed to download media from 360 dialog: {resp.status_code} - {resp.text}")
+            json_resp = resp.json()
+            download_url = json_resp.get("url")
+            download_url = download_url.replace('https://lookaside.fbsbx.com', 'https://waba-v2.360dialog.io')
+            mime_type = json_resp.get("mime_type")
+            extension = mimetypes.guess_extension(mime_type) or ''
+            file_path = f"whataspp_360_{whatsapp_media_id}{extension}"
+        elif provider == 'meta':
+            endpoint = f'https://graph.facebook.com/v22.0/{whatsapp_media_id}'
+            access_token = config.get('access_token')
+            headers = {'Authorization': f'Bearer {access_token}'}
+            media_info_resp = requests.get(
+                endpoint,
+                params={"fields": "url", "access_token": access_token},
+                timeout=10
+            )
+            if media_info_resp.status_code != 200:
+                raise AppException(f"Failed to get url from meta for media: {whatsapp_media_id}")
+            json_resp = media_info_resp.json()
+            download_url = json_resp.get("url")
+            mime_type = json_resp.get("mime_type")
+            extension = mimetypes.guess_extension(mime_type) or ''
+            file_path = f"whataspp_meta_{whatsapp_media_id}{extension}"
+
+        media_resp = requests.get(
+            download_url,
+            headers=headers,
+            stream=True,
+            timeout=10
+        )
+        if media_resp.status_code != 200:
+            raise AppException(f"Failed to download media: {whatsapp_media_id}")
+        buffer = bytearray()
+        for chunk in media_resp.iter_content(chunk_size=8192):
+            if chunk:
+                buffer.extend(chunk)
+        file_buffer = bytes(buffer)
+
+        media_id = uuid7().hex
+        UserMedia.create_user_media_data(
+            bot=bot,
+            media_id=media_id,
+            filename=file_path,
+            sender_id=sender_id,
+            upload_type=UserMediaUploadType.user_uploaded.value)
+
+        asyncio.create_task(UserMedia.save_media_content_task(
+                bot=bot,
+                sender_id=sender_id,
+                media_id=media_id,
+                binary_data=file_buffer,
+                filename=file_path
+            ))
+
+        return [media_id]
+
+
+    @staticmethod
     async def save_media_content_task(
             bot: str,
             sender_id: str,
@@ -122,6 +217,13 @@ class UserMedia:
             sender_id: str,
             files: list[File],
     ):
+        """
+        Upload media contents to cloud storage via background task.
+        :param bot: bot name
+        :param sender_id: sender id
+        :param files: list of files to upload
+        :return: list of media ids
+        """
         media_ids = []
         read_tasks = [asyncio.create_task(file.read()) for file in files]
         binary_datas = await asyncio.gather(*read_tasks)
