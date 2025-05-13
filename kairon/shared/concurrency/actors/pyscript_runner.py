5597709356ff64dd1c31d78b484f64cea209b6f5
rexa0310 <mayankgoyal0310@gmail.com>
2025-05-13 16:30:12 +0530

    added orjson in main pyscript

--- kairon/shared/concurrency/actors/pyscript_runner.py
+++ kairon/shared/concurrency/actors/pyscript_runner.py
@@ -7,7 +7,6 @@ from AccessControl.ZopeGuards import _safe_globals
 from RestrictedPython import compile_restricted
 from RestrictedPython.Guards import safer_getattr
 from loguru import logger
-from orjson import orjson
 from timeout_decorator import timeout_decorator
 
 from kairon.exceptions import AppException
@@ -21,11 +20,11 @@ allow_module("time")
 allow_module("googlemaps")
 allow_module("decimal")
 allow_module("_strptime")
+allow_module("orjson")
 
 global_safe = _safe_globals
 global_safe['_getattr_'] = safer_getattr
 global_safe['json'] = json
-global_safe['orjson'] = orjson
 global_safe['srtp_time'] = PyscriptUtility.srtptime
 global_safe['srtf_time'] = PyscriptUtility.srtftime
 global_safe['url_parse'] = PyscriptUtility.url_parse_quote_plus
