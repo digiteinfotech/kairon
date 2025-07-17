9d544ef7532f7d018530d91e2a2491572f58363a
Aniket Kharkia <aniket.kharkia@nimblework.com>
2025-07-17 13:15:45 +0530

    Updated the code to remove default intents from intents section and fixed the test case for that

--- kairon/shared/constants.py
+++ kairon/shared/constants.py
@@ -4,6 +4,8 @@ from kairon.shared.data.constant import ACCESS_ROLES
 
 DEFAULT_INTENTS = {'restart', 'back', 'out_of_scope', 'session_start', 'nlu_fallback',"k_multimedia_msg"}
 
+EXCLUDED_INTENTS = {'restart', 'back', 'out_of_scope', 'session_start'}
+
 DEFAULT_ACTIONS = {'action_listen', 'action_restart', 'action_session_start', 'action_default_fallback',
                    'action_deactivate_loop', 'action_revert_fallback_events', 'action_default_ask_affirmation',
                    'action_default_ask_rephrase', 'action_two_stage_fallback', 'action_back', '...'}
