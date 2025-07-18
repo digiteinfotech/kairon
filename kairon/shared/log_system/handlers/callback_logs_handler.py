a32bd51a62a280f9b060d4b22e802181bdfe8320
rexa0310 <mayankgoyal0310@gmail.com>
2025-07-18 16:25:50 +0530

    added log handler functionality

---
+++ kairon/shared/log_system/handlers/callback_logs_handler.py
@@ -0,0 +1,17 @@
+import json
+
+from kairon.shared.log_system.base import BaseLogHandler
+
+
+class CallbackLogHandler(BaseLogHandler):
+    def get_logs_and_count(self):
+        from kairon.shared.log_system.executor import LogExecutor
+        query = {"bot": self.bot}
+        for field in ["name", "sender_id", "channel", "identifier"]:
+            value = self.kwargs.get(field)
+            if value:
+                query["callback_name" if field == "name" else field] = value
+        logs_cursor = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("id", "bot", "user", "_id")
+        logs = json.loads(logs_cursor.to_json())
+        count = LogExecutor.get_logs_count(self.doc_type, **query)
+        return logs, count
