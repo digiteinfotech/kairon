2711608da32a9f503e09bcb524e68f0a362b1d37
rexa0310 <mayankgoyal0310@gmail.com>
2025-07-29 11:21:03 +0530

    added search logs api

--- kairon/shared/log_system/handlers/audit_logs_handler.py
+++ kairon/shared/log_system/handlers/audit_logs_handler.py
@@ -17,3 +17,26 @@ class AuditLogHandler(BaseLogHandler):
 
         count = self.get_logs_count(self.doc_type, **query)
         return logs, count
+
+    def get_logs_for_search_query(self):
+        from_date = self.kwargs.pop("from_date", datetime.utcnow().date())
+        to_date = self.kwargs.pop("to_date", from_date + timedelta(days=1))
+        to_date += timedelta(days=1)
+
+        query = {
+            "attributes__key": "bot",
+            "attributes__value": self.bot,
+            "timestamp__gte": from_date,
+            "timestamp__lte": to_date
+        }
+
+        query.update(self.kwargs)
+
+        logs_cursor = (
+            self.doc_type.objects(**query)
+            .order_by("-timestamp")
+            .exclude("id")
+        )
+        logs = json.loads(logs_cursor.to_json())
+        count = self.get_logs_count(self.doc_type, **query)
+        return logs, count
