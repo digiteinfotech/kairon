1d64b907bd42a5ab412f1ea60c7edca7ace954f1
rexa0310 <mayankgoyal0310@gmail.com>
2025-07-31 11:34:32 +0530

    search logs api

--- kairon/shared/log_system/handlers/executor_logs_handler.py
+++ kairon/shared/log_system/handlers/executor_logs_handler.py
@@ -37,7 +37,7 @@ class ExecutorLogHandler(BaseLogHandler):
             .order_by("-timestamp")
             .exclude("_id")
         )
-        logs = json.loads(logs_cursor.to_json())
-        count = self.get_logs_count(self.doc_type, **query)
+        logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
+        count = self.get_logs_count(self.doc_type,  **query)
         return logs, count
 
