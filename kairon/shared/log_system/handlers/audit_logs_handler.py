99f921c306b36c44f17415094538d1d11368ab9d
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-08-19 18:41:16 +0530

    i added from date and to date in handlers for fetching the logs and

--- kairon/shared/log_system/handlers/audit_logs_handler.py
+++ kairon/shared/log_system/handlers/audit_logs_handler.py
@@ -6,12 +6,10 @@ class AuditLogHandler(BaseLogHandler):
     def get_logs_and_count(self):
         now = datetime.utcnow()
 
-        # Start of the month
         from_date = self.kwargs.get("from_date") or now.replace(
             day=1, hour=0, minute=0, second=0, microsecond=0
         )
 
-        # End of the month
         last_day = calendar.monthrange(now.year, now.month)[1]
         to_date = self.kwargs.get("to_date") or now.replace(
             day=last_day, hour=23, minute=59, second=59, microsecond=999999
@@ -22,7 +20,7 @@ class AuditLogHandler(BaseLogHandler):
             "timestamp__gte": from_date,
             "timestamp__lte": to_date
         }
-        logs_json = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("bot")
+        logs_json = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("id")
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_json)
 
         count = self.get_logs_count(self.doc_type, **query)
