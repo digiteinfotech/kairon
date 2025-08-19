a2191a5f73f28f77bb3584f5975d0c7c80e9d36e
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-08-19 14:54:08 +0530

    i added from date and to date in handlers for fetching the logs

--- kairon/shared/log_system/handlers/model_testing_logs_handler.py
+++ kairon/shared/log_system/handlers/model_testing_logs_handler.py
@@ -1,10 +1,21 @@
 from ..base import BaseLogHandler
 
 from ...test.data_objects import ModelTestingLogs
-
+from datetime import datetime
+import calendar
 
 class ModelTestingHandler(BaseLogHandler):
     def get_logs_and_count(self):
+        from_date = self.kwargs.get("from_date") or datetime.utcnow().replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0
+        )
+
+        to_date = self.kwargs.get("to_date") or from_date.replace(
+            day=calendar.monthrange(from_date.year, from_date.month)[1],
+            hour=23, minute=59, second=59, microsecond=999999
+        )
+
+
         logs = list(ModelTestingLogs.objects(bot=self.bot).aggregate([
             {"$set": {"data.type": "$type"}},
             {'$group': {'_id': '$reference_id', 'bot': {'$first': '$bot'}, 'user': {'$first': '$user'},
@@ -21,7 +32,32 @@ class ModelTestingHandler(BaseLogHandler):
                 'is_augmented': 1
             }},
             {"$sort": {"start_timestamp": -1}}]))[self.start_idx:self.start_idx + self.page_size]
-        query={"bot": self.bot,
-               "type": "common"}
+
+
+        query = {
+            "bot": self.bot,
+            "type": "common",
+            "start_timestamp__gte": from_date,
+            "start_timestamp__lte": to_date
+        }
         count = self.get_logs_count(self.doc_type, **query)
+        return logs, count
+
+    def get_logs_for_search_query(self):
+        from_date = self.kwargs.pop("from_date", None)
+        to_date = self.kwargs.pop("to_date", None)
+
+        query = {"bot": self.bot}
+
+        if from_date:
+            query["start_timestamp__gte"] = from_date
+        if to_date:
+            query["start_timestamp__lte"] = to_date
+
+        query.update(self.kwargs)
+        print(query)
+
+        logs_cursor = (self.doc_type.objects(**query).order_by("-start_timestamp").skip(self.start_idx).limit(self.page_size).exclude("id"))
+        logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
+        count = self.get_logs_count(self.doc_type,  **query)
         return logs, count
