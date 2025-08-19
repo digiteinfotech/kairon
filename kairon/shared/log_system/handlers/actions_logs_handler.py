a738033505d94f27b13b4545752ecbbcee6c9a18
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-08-19 14:54:08 +0530

    i added from date and to date in handlers for fetching the logs

--- kairon/shared/log_system/handlers/actions_logs_handler.py
+++ kairon/shared/log_system/handlers/actions_logs_handler.py
@@ -1,9 +1,25 @@
 
 from kairon.shared.log_system.base import BaseLogHandler
+from datetime import datetime
+import calendar
 
 class ActionLogHandler(BaseLogHandler):
     def get_logs_and_count(self):
-        query = {"bot": self.bot, "trigger_info__trigger_id": ""}
+        from_date = self.kwargs.get("from_date") or datetime.utcnow().replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0
+        )
+
+        to_date = self.kwargs.get("to_date") or from_date.replace(
+            day=calendar.monthrange(from_date.year, from_date.month)[1],
+            hour=23, minute=59, second=59, microsecond=999999
+        )
+        query = {
+            "bot": self.bot,
+            "timestamp__gte": from_date,
+            "timestamp__lte": to_date
+        }
+
+
         logs_cursor = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("bot")
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
         count = self.get_logs_count(self.doc_type, **query)
@@ -25,7 +41,7 @@ class ActionLogHandler(BaseLogHandler):
 
         query.update(self.kwargs)
 
-        logs_cursor = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("bot")
+        logs_cursor = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("id")
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
         count = self.get_logs_count(self.doc_type,  **query)
         return logs, count
--- kairon/shared/log_system/handlers/executor_logs_handler.py
+++ kairon/shared/log_system/handlers/executor_logs_handler.py
@@ -1,10 +1,25 @@
 from kairon.shared.log_system.base import BaseLogHandler
-
+import calendar
+from datetime import datetime
 
 
 class ExecutorLogHandler(BaseLogHandler):
     def get_logs_and_count(self):
-        query = {"bot": self.bot}
+        from_date = self.kwargs.get("from_date") or datetime.utcnow().replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0
+        )
+
+        to_date = self.kwargs.get("to_date") or from_date.replace(
+            day=calendar.monthrange(from_date.year, from_date.month)[1],
+            hour=23, minute=59, second=59, microsecond=999999
+        )
+
+        query = {
+            "bot": self.bot,
+            "timestamp__gte": from_date,
+            "timestamp__lte": to_date
+        }
+
         for field in ["event_class", "task_type"]:
             value = self.kwargs.get(field)
             if value:
@@ -27,13 +42,7 @@ class ExecutorLogHandler(BaseLogHandler):
 
         query.update(self.kwargs)
 
-        logs_cursor = (
-            self.doc_type.objects(**query)
-            .order_by("-timestamp")
-            .skip(self.start_idx)
-            .limit(self.page_size)
-            .exclude("_id")
-        )
+        logs_cursor = (self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("_id"))
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
         count = self.get_logs_count(self.doc_type,  **query)
         return logs, count
