50d5e783c589b9e96f6f3d104b42dcbe6c21ecb6
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-08-21 14:33:53 +0530

    i added from date and to date in handlers for fetching the logs and

--- kairon/shared/log_system/handlers/audit_logs_handler.py
+++ kairon/shared/log_system/handlers/audit_logs_handler.py
@@ -38,7 +38,7 @@ class AuditLogHandler(BaseLogHandler):
         }
         query.update(self.kwargs)
 
-        logs_cursor = (self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("id"))
+        logs_cursor = (self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("bot"))
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
         count = self.get_logs_count(self.doc_type,  **query)
         return logs, count
