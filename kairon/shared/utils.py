fa002b0e452e85156f563ed8722d388f325610d7
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-08-22 12:53:07 +0530

    i added from date and to date in handlers for fetching the logs and

--- kairon/shared/utils.py
+++ kairon/shared/utils.py
@@ -156,6 +156,9 @@ class Utility:
             return True
         else:
             return False
+    @staticmethod
+    def check_from_date_less_than_to_date(from_date,to_date):
+        return from_date>to_date
 
     @staticmethod
     def check_character_limit(value: str):
