4323d9358cf926d182c3c3e8907bda9fbdaf8799
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-08-25 21:26:44 +0530

    i added from date and to date in handlers for fetching the logs and

--- kairon/shared/log_system/handlers/actions_logs_handler.py
+++ kairon/shared/log_system/handlers/actions_logs_handler.py
@@ -18,8 +18,6 @@ class ActionLogHandler(BaseLogHandler):
             "timestamp__gte": from_date,
             "timestamp__lte": to_date
         }
-
-
         logs_cursor = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("bot")
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
         count = self.get_logs_count(self.doc_type, **query)
@@ -28,7 +26,6 @@ class ActionLogHandler(BaseLogHandler):
     def get_logs_for_search_query(self):
         from_date = self.kwargs.pop("from_date", None)
         to_date = self.kwargs.pop("to_date", None)
-
         query = {
             "bot": self.bot,
             "trigger_info__trigger_id": ""
@@ -40,9 +37,7 @@ class ActionLogHandler(BaseLogHandler):
             query["timestamp__lte"] = to_date + timedelta(days=1)
 
         query.update(self.kwargs)
-
         logs_cursor = self.doc_type.objects(**query).order_by("-timestamp").skip(self.start_idx).limit(self.page_size).exclude("id")
         logs = BaseLogHandler.convert_logs_cursor_to_dict(logs_cursor)
         count = self.get_logs_count(self.doc_type,  **query)
         return logs, count
-
