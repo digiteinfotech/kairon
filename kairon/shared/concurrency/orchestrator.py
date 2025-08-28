6618f7c0fe8dfd38d3ce641d7b9469c80f505be8
Mayank Goyal <67274027+rexa0310@users.noreply.github.com>
2025-08-28 14:23:22 +0530

    added a retry mechanism (#2116)
    * added a retry mechanism
    
    * added a retry mechanism

--- kairon/shared/concurrency/orchestrator.py
+++ kairon/shared/concurrency/orchestrator.py
@@ -6,17 +6,26 @@ from kairon.exceptions import AppException
 from kairon.shared.concurrency.actors.factory import ActorFactory
 
 
+
 class ActorOrchestrator:
 
     @staticmethod
     def run(actor_type: Text, *args, **kwargs):
         actor = ActorFactory.get_instance(actor_type)
         actor_timeout = kwargs.get("timeout")
+        retries = kwargs.get("retries", 1)
         try:
-            future = actor.execute(*args, **kwargs)
-            result = future.get(actor_timeout)
-            return result
-        except pykka._exceptions.Timeout as e:
-            raise AppException(f"Operation timed out: {e}")
+            for attempt in range(1, retries + 1):
+                try:
+                    future = actor.execute(*args, **kwargs)
+                    result = future.get(actor_timeout)
+                    return result
+                except pykka._exceptions.Timeout as e:
+                    raise AppException(f"Operation timed out: {e}")
+                except Exception as e:
+                    if attempt == retries:
+                        raise AppException(
+                            str(e)
+                        )
         finally:
             actor.actor_ref.stop()
