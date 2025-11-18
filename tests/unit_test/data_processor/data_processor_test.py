2156178bdc282328d0304c3f2052920851fffc8b
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-11-18 10:46:42 +0530

    update for delete chat conversations

--- tests/unit_test/data_processor/data_processor_test.py
+++ tests/unit_test/data_processor/data_processor_test.py
@@ -5206,6 +5206,7 @@ class TestMongoProcessor:
         user = 'test_user'
         processor = CognitionDataProcessor()
         response = DataProcessor.get_collection_data_with_id(bot, collection_id=pytest.collection_id)
+        print(response)
         assert response == {
             '_id': pytest.collection_id,
             'collection_name': 'user',
