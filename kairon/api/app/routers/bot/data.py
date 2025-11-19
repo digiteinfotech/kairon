16d9be861c1ff53a591e0a3d6c7cfa89f73bef61
ganesh_reddy <ganeshreddyk8g@gmail.com>
2025-11-19 12:22:33 +0530

    added pagination for the custom collection and update for delete chat conversations

--- kairon/api/app/routers/bot/data.py
+++ kairon/api/app/routers/bot/data.py
@@ -291,7 +291,6 @@ async def get_collection_data(
     """
     Fetches collection data based on the multiple filters provided
     """
-    print("hello")
     return {"data": list(DataProcessor.get_collection_data(current_user.get_bot(),
                                                                  collection_name=collection_name,
                                                                  key=key, value=value))}
