c51195b329da1569d3007c6bb9e860eea647473d
Mahesh Sattala <mahesh.sattala@digite.com>
2025-11-24 14:32:57 +0530

    made some changes in odoo code

---
+++ kairon/shared/pos/constants.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class POSType(str, Enum):
+    odoo = "odoo"
+
+
+#   ACTIONS
+ACTION_POS_PRODUCT_LIST = 388
+ACTION_POS_ORDER_LIST = 380
+
+
+#   MENUS
+MENU_POS_PRODUCTS = 233
+MENU_POS_ORDERS = 231
+
