21dc6edd5f8cb9d56bf2c55d4405b2f86e38a53b
Aniketkharkia <141703736+Aniketkharkia@users.noreply.github.com>
2025-12-30 10:24:52 +0530

    integrating llama models (#2296)
    * integrating llama models
    
    * integrating llama models
    
    * integrating llama models
    
    * integrating llama models
    
    ---------
    
    Co-authored-by: Aniket Kharkia <aniket.kharkia@nimblework.com>

--- tests/unit_test/data_processor/action_serializer_test.py
+++ tests/unit_test/data_processor/action_serializer_test.py
@@ -793,7 +793,8 @@ def test_get_model_llm_type_map_dynamic():
         'anthropic': ['bedrock/us.anthropic', 'claude'],
         'gemini': ['gemini/'],
         'perplexity': ['perplexity/llama'],
-        'aws-nova': ['bedrock/converse/us.amazon.nova']
+        'aws-nova': ['bedrock/converse/us.amazon.nova'],
+        'aws-llama': ['bedrock/us.meta.llama4-maverick', 'bedrock/us.meta.llama4-scout']
     }
 
     for model_name, provider in result.items():
