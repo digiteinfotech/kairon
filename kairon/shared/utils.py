e9246fa97ffec824284eb483f61b85c085824dd1
ganesh_reddy <ganeshreddyk8g@gmail.com>
2026-01-05 10:25:54 +0530

    in file upload while uploading two files simultaneously its raising error.

--- kairon/shared/utils.py
+++ kairon/shared/utils.py
@@ -413,6 +413,23 @@ class Utility:
         return max(files, key=os.path.getctime)
 
     @staticmethod
+    def get_latest_file1(folder, extension_pattern="*"):
+        """
+        Fetches latest file.
+        If extension is provided, latest file with that extension is retrieved.
+        By default, latest file in the folder is retrieved and can be of any type.
+        Example extension patterns: "*.tar.gz", "*.zip", etc.
+
+        :param folder: folder path
+        :param extension_pattern: file extension as a regular expression
+        :return: latest file
+        """
+        logger.info(f"Model path: {folder}")
+        if not os.path.exists(folder):
+            raise AppException("Folder does not exists!")
+        return max(iglob(os.path.join(folder, extension_pattern)), key=os.path.getctime)
+
+    @staticmethod
     def deploy_model(endpoint: Dict, bot: Text):
         """
         deploys the model to the specified endpoint
