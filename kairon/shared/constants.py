2ab37ca7747c9fb5ccbe5fda5f7dc046c08d05d2
mahesh_sattala <mahesh.sattala@digite.com>
2026-02-25 10:35:36 +0530

    added constant FLATTENED_CONVERSATIONS

--- kairon/shared/constants.py
+++ kairon/shared/constants.py
@@ -31,6 +31,7 @@ KAIRON_USER_MSG_ENTITY = "kairon_user_msg"
 
 FLATTENED_CONVERSATIONS = "flattened_conversations"
 
+
 FAQ_DISABLED_ERR = "Faq feature is disabled for the bot! Please contact support."
 
 
